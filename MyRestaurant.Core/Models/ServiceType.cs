8b3543cae988c1302d5313686becf68bf7b87161
math2018avan <math2018avan@gmail.com>
2020-12-22 18:01:38 +0800

    Models
    Models added

---
+++ MyRestaurant.Core/Models/ServiceType.cs
@@ -0,0 +1,8 @@
+﻿namespace MyRestaurant.Core.Models
+{
+    public class ServiceType : MyRestaurantBase
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+    }
+}
