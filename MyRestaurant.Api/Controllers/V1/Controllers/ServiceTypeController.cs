7c0dec48a58c2b6c835870cb9f2544a499001e56
math2018avan <math2018avan@gmail.com>
2020-12-22 18:04:22 +0800

    Create ServiceTypeController.cs
    simple controller added

---
+++ MyRestaurant.Api/Controllers/V1/Controllers/ServiceTypeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyRestaurant.Business.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Api.Controllers.V1.Controllers
+{
+    [ApiVersion("1.0")]
+    public class ServiceTypeController : BaseController
+    {
+
+        private readonly IServiceTypeRepository _serviceTypeRepository;
+        public ServiceTypeController(IServiceTypeRepository serviceTypeRepository)
+        {
+            _serviceTypeRepository = serviceTypeRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetServiceTypes()
+        {
+            var result = await _serviceTypeRepository.GetServicesTypeAsync();
+            return Ok(result);
+        }
+    }
+}
