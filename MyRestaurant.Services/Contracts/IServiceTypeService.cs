84ce67cef81fa111b8caed1d50629e1836811bcf
math2018avan <math2018avan@gmail.com>
2020-12-24 18:21:29 +0800

    add more service methods
    More functions added

--- MyRestaurant.Services/Contracts/IServiceTypeService.cs
+++ MyRestaurant.Services/Contracts/IServiceTypeService.cs
@@ -7,5 +7,9 @@ namespace MyRestaurant.Services.Contracts
     public interface IServiceTypeService
     {
         Task<IEnumerable<ServiceType>> GetServiceTypesAsync();
+        Task<ServiceType> GetServiceTypeAsync(int id);
+        Task AddServiceTypeAsync(ServiceType serviceType);
+        Task UpdateServiceTypeAsync(ServiceType serviceType);
+        Task DeleteServiceTypeAsync(ServiceType serviceType);
     }
 }
--- MyRestaurant.Services/ServiceTypeService.cs
+++ MyRestaurant.Services/ServiceTypeService.cs
@@ -13,9 +13,32 @@ namespace MyRestaurant.Services
         {
             _context = context;
         }
+
+        public async Task<ServiceType> GetServiceTypeAsync(int id)
+        {
+            return await _context.GetFirstOrDefaultAsync<ServiceType>(d => d.Id == id);
+        }
+        public async Task AddServiceTypeAsync(ServiceType serviceType)
+        {
+            await _context.InsertAsync(serviceType);
+            await _context.CommitAsync();
+        }
+
+        public async Task DeleteServiceTypeAsync(ServiceType serviceType)
+        {
+            _context.Delete(serviceType);
+            await _context.CommitAsync();
+        }
+
         public async Task<IEnumerable<ServiceType>> GetServiceTypesAsync()
         {
             return await _context.GetAllAsync<ServiceType>();
+        }
+
+        public async Task UpdateServiceTypeAsync(ServiceType serviceType)
+        {
+            _context.Modify(serviceType);
+            await _context.CommitAsync();
         }
     }
 }
