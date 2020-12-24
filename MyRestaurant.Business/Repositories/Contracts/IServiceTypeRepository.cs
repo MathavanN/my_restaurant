c0f80b3107f9747b3e9a4b0e4414933a6eadc1f8
math2018avan <math2018avan@gmail.com>
2020-12-24 18:22:45 +0800

    more repository methods added
    more repository methods added

--- MyRestaurant.Business/Repositories/Contracts/IServiceTypeRepository.cs
+++ MyRestaurant.Business/Repositories/Contracts/IServiceTypeRepository.cs
@@ -1,4 +1,4 @@
-﻿using MyRestaurant.Models;
+﻿using MyRestaurant.Business.Dtos.V1.ServiceTypeDtos;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -6,6 +6,10 @@ namespace MyRestaurant.Business.Repositories.Contracts
 {
     public interface IServiceTypeRepository
     {
-        Task<IEnumerable<ServiceType>> GetServicesTypeAsync();
+        Task<IEnumerable<GetServiceTypeDto>> GetServicesTypesAsync();
+        Task<GetServiceTypeDto> GetServiceTypeAsync(int id);
+        Task<GetServiceTypeDto> CreateServiceTypeAsync(CreateServiceTypeDto serviceTypeDto);
+        Task UpdateServiceTypeAsync(int id, EditServiceTypeDto serviceTypeDto);
+        Task DeleteServiceTypeAsync(int id);
     }
 }
