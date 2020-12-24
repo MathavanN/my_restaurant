0dd8f1c829442e89762dfe854a774376da71cc8b
math2018avan <math2018avan@gmail.com>
2020-12-24 18:22:06 +0800

    Create AutoMapping.cs
    automapper added

---
+++ MyRestaurant.Business/AutoMapping/AutoMapping.cs
@@ -0,0 +1,19 @@
+﻿using MyRestaurant.Business.Dtos.V1.ServiceTypeDtos;
+using MyRestaurant.Models;
+
+namespace MyRestaurant.Business.AutoMapping
+{
+    public class AutoMapping : AutoMapper.Profile
+    {
+        public AutoMapping()
+        {
+            //map from payload to models
+            CreateMap<CreateServiceTypeDto, ServiceType>();
+            CreateMap<EditServiceTypeDto, ServiceType>();
+
+
+            //map from models to dto
+            CreateMap<ServiceType, GetServiceTypeDto>();
+        }
+    }
+}
