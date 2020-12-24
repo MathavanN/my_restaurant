60a19133204451e9664fbdc1ddaa0f22d92532ad
math2018avan <math2018avan@gmail.com>
2020-12-24 15:04:12 +0800

    Sedrvice Project
    New Service Project added

---
+++ MyRestaurant.Services/Contracts/IServiceTypeService.cs
@@ -0,0 +1,11 @@
+﻿using MyRestaurant.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Services.Contracts
+{
+    public interface IServiceTypeService
+    {
+        Task<IEnumerable<ServiceType>> GetServiceTypesAsync();
+    }
+}
