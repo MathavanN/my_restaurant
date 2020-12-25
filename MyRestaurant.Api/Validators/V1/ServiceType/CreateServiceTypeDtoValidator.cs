f470135a9ea79284906f89fab8c8ca5790b72bf2
math2018avan <math2018avan@gmail.com>
2020-12-25 22:28:38 +0800

    Create CreateServiceTypeDtoValidator.cs
    Dto Validator added

---
+++ MyRestaurant.Api/Validators/V1/ServiceType/CreateServiceTypeDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using MyRestaurant.Business.Dtos.V1.ServiceTypeDtos;
+
+namespace MyRestaurant.Api.Validators.V1.ServiceType
+{
+    public class CreateServiceTypeDtoValidator : AbstractValidator<CreateServiceTypeDto>
+    {
+        public CreateServiceTypeDtoValidator()
+        {
+            RuleFor(x => x.Type).NotEmpty().WithMessage("Service type is required.")
+                .MaximumLength(20).WithMessage("Service type maximum length is 20.");
+        }
+    }
+}
