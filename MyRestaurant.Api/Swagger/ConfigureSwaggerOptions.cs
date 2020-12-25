ac86cf4022e58e64483e4239351d3cf2bd88b753
math2018avan <math2018avan@gmail.com>
2020-12-25 22:29:31 +0800

    Update ConfigureSwaggerOptions.cs
    code added to display xml comments to show in swagged, but disabled

--- MyRestaurant.Api/Swagger/ConfigureSwaggerOptions.cs
+++ MyRestaurant.Api/Swagger/ConfigureSwaggerOptions.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
+using System.IO;
+using System.Reflection;
 
 namespace MyRestaurant.Api.Swagger
 {
@@ -19,6 +21,11 @@ namespace MyRestaurant.Api.Swagger
             {
                 options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
             }
+
+            // Set the comments path for the Swagger JSON and UI.
+            //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            //options.IncludeXmlComments(xmlPath);
         }
 
         static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
