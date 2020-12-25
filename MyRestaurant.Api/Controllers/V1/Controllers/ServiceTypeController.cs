ad5c5c383fb6a7da4f4383ea85a17ee4d4ab4fb6
math2018avan <math2018avan@gmail.com>
2020-12-25 22:31:09 +0800

    Update ServiceTypeController.cs
    error response type changed

--- MyRestaurant.Api/Controllers/V1/Controllers/ServiceTypeController.cs
+++ MyRestaurant.Api/Controllers/V1/Controllers/ServiceTypeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MyRestaurant.Business.Dtos.V1.ServiceTypeDtos;
 using MyRestaurant.Business.Repositories.Contracts;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace MyRestaurant.Api.Controllers.V1.Controllers
@@ -26,7 +27,7 @@ namespace MyRestaurant.Api.Controllers.V1.Controllers
 
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetServiceType(int id)
         {
             var result = await _serviceTypeRepository.GetServiceTypeAsync(id);
@@ -35,6 +36,8 @@ namespace MyRestaurant.Api.Controllers.V1.Controllers
 
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), StatusCodes.Status409Conflict)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> CreateServiceType(CreateServiceTypeDto serviceTypeDto, ApiVersion version)
         {
             var result = await _serviceTypeRepository.CreateServiceTypeAsync(serviceTypeDto);
@@ -43,14 +46,24 @@ namespace MyRestaurant.Api.Controllers.V1.Controllers
 
         [HttpPut("{id}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), StatusCodes.Status404NotFound)]
         public async Task<IActionResult> UpdateServiceType(int id,  EditServiceTypeDto serviceTypeDto) 
         {
             await _serviceTypeRepository.UpdateServiceTypeAsync(id, serviceTypeDto);
             return NoContent();
         }
 
+        /// <summary>
+        /// Delete a specific ServiceType
+        /// </summary>
+        /// <param name="id">ServiceType id</param>
+        /// <returns></returns>
+        /// <response code="204">Returns no content</response>
+        /// <response code="404">If the specified ServiceType id is not available</response> 
         [HttpDelete("{id}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteServiceType(int id)
         {
             await _serviceTypeRepository.DeleteServiceTypeAsync(id);
