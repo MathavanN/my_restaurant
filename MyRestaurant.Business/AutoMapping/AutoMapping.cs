e042dab63e2cd73e4e38b6c318900933eae6c764
math2018avan <math2018avan@gmail.com>
2020-12-26 13:45:27 +0800

    Update AutoMapping.cs
    restaurantInfo mapping added

--- MyRestaurant.Business/AutoMapping/AutoMapping.cs
+++ MyRestaurant.Business/AutoMapping/AutoMapping.cs
@@ -1,4 +1,4 @@
-﻿using MyRestaurant.Business.Dtos.V1.ServiceTypeDtos;
+﻿using MyRestaurant.Business.Dtos.V1;
 using MyRestaurant.Models;
 
 namespace MyRestaurant.Business.AutoMapping
@@ -10,10 +10,13 @@ namespace MyRestaurant.Business.AutoMapping
             //map from payload to models
             CreateMap<CreateServiceTypeDto, ServiceType>();
             CreateMap<EditServiceTypeDto, ServiceType>();
+            CreateMap<CreateRestaurantInfoDto, RestaurantInfo>();
 
 
             //map from models to dto
             CreateMap<ServiceType, GetServiceTypeDto>();
+            CreateMap<RestaurantInfo, GetRestaurantInfoDto>()
+                .ForMember(d => d.Address, opt => opt.MapFrom(src => $"{src.Address}, {src.City}, {src.Country}"));
         }
     }
 }
