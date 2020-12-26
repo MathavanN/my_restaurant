6c3c581c78af176f123f3c7ed6142840b3de8ac1
math2018avan <math2018avan@gmail.com>
2020-12-26 13:41:50 +0800

    RestaurantInfo mapping
    RestaurantInfo mapping added

--- MyRestaurant.Core/Configurations/Base/MappingBase.cs
+++ MyRestaurant.Core/Configurations/Base/MappingBase.cs
@@ -4,4 +4,5 @@ namespace MyRestaurant.Core.Configurations.Base
 {
     public abstract class AuditMappingBase : BaseEntityTypeConfiguration<Audit> { }
     public abstract class ServiceTypeMappingBase : BaseEntityTypeConfiguration<ServiceType> { }
+    public abstract class RestaurantInfoMappingBase : BaseEntityTypeConfiguration<RestaurantInfo> { }
 }
---
+++ MyRestaurant.Core/Configurations/Mapping/RestaurantInfoMapping.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyRestaurant.Core.Configurations.Base;
+using MyRestaurant.Models;
+
+namespace MyRestaurant.Core.Configurations.Mapping
+{
+    internal class RestaurantInfoMapping : RestaurantInfoMappingBase
+    {
+        public override void Configure(EntityTypeBuilder<RestaurantInfo> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Id).UseIdentityColumn();
+            builder.Property(t => t.Name).HasColumnType("varchar(256)").IsRequired();
+            builder.Property(t => t.Address).HasColumnType("varchar(256)").IsRequired();
+            builder.Property(t => t.City).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(t => t.Country).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(t => t.LandLine).HasColumnType("varchar(20)");
+            builder.Property(t => t.Mobile).HasColumnType("varchar(20)");
+            builder.Property(t => t.Email).HasColumnType("varchar(256)");
+            builder.ToTable("RestaurantInfos");
+            base.Configure(builder);
+        }
+    }
+}
