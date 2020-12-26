ffac7ecad588d21244ee4c46b8ca1d838824d0a2
math2018avan <math2018avan@gmail.com>
2020-12-26 13:42:34 +0800

    RestaurantInfo migration
    RestaurantInfo migration added

--- MyRestaurant.Core/Migrations/20201226042724_RestaurantInfoAdded.Designer.cs
+++ MyRestaurant.Core/Migrations/20201226042724_RestaurantInfoAdded.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyRestaurant.Core;
 
 namespace MyRestaurant.Core.Migrations
 {
     [DbContext(typeof(MyRestaurantContext))]
-    partial class MyRestaurantContextModelSnapshot : ModelSnapshot
+    [Migration("20201226042724_RestaurantInfoAdded")]
+    partial class RestaurantInfoAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
