9105141ed840e9666ae18275e392697c1bb39ec1
math2018avan <math2018avan@gmail.com>
2020-12-26 13:45:13 +0800

    restaurantInfo dto
    restaurantInfo dto created

---
+++ MyRestaurant.Business/Dtos/V1/RestaurantInfoDtos/CreateRestaurantInfoDto.cs
@@ -0,0 +1,15 @@
+﻿namespace MyRestaurant.Business.Dtos.V1
+{
+    public class CreateRestaurantInfoDto
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string LandLine { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+    }
+
+
+}
