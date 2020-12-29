3df41ddd7fa492da6dd9712078afd200cc680f49
math2018avan <math2018avan@gmail.com>
2020-12-29 13:10:42 +0800

    Update Program.cs
    Database seed added

--- MyRestaurant.Api/Program.cs
+++ MyRestaurant.Api/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MyRestaurant.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +15,29 @@ namespace MyRestaurant.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            SeedDatabase(host);
+
+            host.Run();
         }
 
+        private static void SeedDatabase(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                try
+                {
+                    MyRestaurantSeedData.Initialize(services).Wait();
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError("An error occurred while seeding the database", ex.Message);
+                }
+            }
+        }
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
