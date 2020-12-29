93f7d9f1cf21c52f04313f2291101cb9d9898a72
math2018avan <math2018avan@gmail.com>
2020-12-29 13:06:47 +0800

    Create BaseApiController.cs
    abstract class created

---
+++ MyRestaurant.Api/Controllers/BaseApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyRestaurant.Api.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public abstract class BaseApiController<T> : ControllerBase
+    {
+    }
+}
