b1efe05060d68f093e870686d9ad602ed178fceb
math2018avan <math2018avan@gmail.com>
2020-12-29 13:12:14 +0800

    jwt token generation
    jwt topken generation

---
+++ MyRestaurant.Services/Account/IAccountService.cs
@@ -0,0 +1,13 @@
+﻿using MyRestaurant.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Services.Account
+{
+    public interface IAccountService
+    {
+        Task<string> GenerateAccessToken(User user);
+        Task<RefreshToken> GenerateRefreshToken(Guid userId, string ipAddress);
+        bool ValidateRefreshToken(string token);
+    }
+}
