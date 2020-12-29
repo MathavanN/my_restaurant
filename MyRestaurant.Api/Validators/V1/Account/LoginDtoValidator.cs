2de5229f148e46e0a3e21e944fc1bf0fe2375d97
math2018avan <math2018avan@gmail.com>
2020-12-29 13:06:00 +0800

    account dto validation

---
+++ MyRestaurant.Api/Validators/V1/Account/LoginDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MyRestaurant.Business.Dtos.V1;
+
+namespace MyRestaurant.Api.Validators.V1
+{
+    public class LoginDtoValidator : AbstractValidator<LoginDto>
+    {
+        public LoginDtoValidator()
+        {
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Email is not a valid email address.");
+
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required.");
+        }
+    }
+
+    public class RefreshDtoValidator : AbstractValidator<RefreshDto>
+    {
+        public RefreshDtoValidator()
+        {
+
+            RuleFor(x => x.RefreshToken).NotEmpty().WithMessage("RefreshToken is required.");
+        }
+    }
+}
---
+++ MyRestaurant.Api/Validators/V1/Account/RegisterDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MyRestaurant.Business.Dtos.V1;
+
+namespace MyRestaurant.Api.Validators.V1
+{
+    public class RegisterDtoValidator : AbstractValidator<RegisterDto>
+    {
+        public RegisterDtoValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty().WithMessage("FirstName is required.");
+
+            RuleFor(x => x.LastName).NotEmpty().WithMessage("LastName is required.");
+
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Email is not a valid email address.");
+
+            RuleFor(x => x.Password).NotEmpty().Equal(x => x.ConfirmPassword).WithMessage("Password must be same.");
+        }
+    }
+}
