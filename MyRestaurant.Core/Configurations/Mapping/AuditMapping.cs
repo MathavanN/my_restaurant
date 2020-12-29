e3f6a5fdada35a258e02aac66710eff4d76ef5b3
math2018avan <math2018avan@gmail.com>
2020-12-29 13:11:43 +0800

    mapping
    audit, refreshToken mapping added

--- MyRestaurant.Core/Configurations/Mapping/AuditMapping.cs
+++ MyRestaurant.Core/Configurations/Mapping/AuditMapping.cs
@@ -17,4 +17,25 @@ namespace MyRestaurant.Core.Configurations.Mapping
             base.Configure(builder);
         }
     }
+
+    internal class RefreshTokenMapping : RefreshTokenMappingBase
+    {
+        public override void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.UserId).IsRequired();
+            builder.Property(t => t.Token).IsRequired();
+            builder.Property(t => t.Expires).IsRequired();
+            builder.Property(t => t.IsExpired).IsRequired();
+            builder.Property(t => t.Created).IsRequired();
+            builder.Property(t => t.CreatedByIp).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(t => t.Revoked);
+            builder.Property(t => t.RevokedByIp).HasColumnType("varchar(100)");
+            builder.Property(t => t.ReplacedByToken).HasColumnType("varchar(100)");
+            builder.ToTable("RefreshTokens");
+
+            builder.HasOne(t => t.User).WithMany(r => r.RefreshTokens).HasForeignKey(f => f.UserId);
+            base.Configure(builder);
+        }
+    }
 }
