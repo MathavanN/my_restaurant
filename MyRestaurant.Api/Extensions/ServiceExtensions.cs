61a8bb1174be487f972d9af292add1f2cc113c73
math2018avan <math2018avan@gmail.com>
2020-12-30 18:53:52 +0800

    Update ServiceExtensions.cs
    authentication and authorization policy added

--- MyRestaurant.Api/Extensions/ServiceExtensions.cs
+++ MyRestaurant.Api/Extensions/ServiceExtensions.cs
@@ -3,6 +3,7 @@ using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
@@ -12,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using MyRestaurant.Api.Middleware;
+using MyRestaurant.Api.PolicyHandlers;
 using MyRestaurant.Api.Swagger;
 using MyRestaurant.Api.Validators.V1;
 using MyRestaurant.Business.Dtos.V1;
@@ -20,18 +22,24 @@ using MyRestaurant.Business.Repositories.Contracts;
 using MyRestaurant.Core;
 using MyRestaurant.Models;
 using MyRestaurant.Services;
-using MyRestaurant.Services.Account;
-using MyRestaurant.Services.Contracts;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MyRestaurant.Api.Extensions
 {
     public static class ServiceExtensions
     {
+        public static void ConfigureAuthorizationHandler(this IServiceCollection services)
+        {
+            services.AddSingleton<IAuthorizationHandler, MyRestaurantAccessHandler>();
+        }
+
         public static void ConfigureAuthorization(this IServiceCollection services)
         {
             services.AddAuthorization(options =>
@@ -39,11 +47,15 @@ namespace MyRestaurant.Api.Extensions
                 options.FallbackPolicy = new AuthorizationPolicyBuilder()
                 .RequireAuthenticatedUser()
                 .Build();
+                options.AddPolicy(ApplicationClaimPolicy.SuperAdminOnly, policy => policy.Requirements.Add(new MyRestaurantAccessRequirement(ApplicationClaimType.SuperAdmin)));
+                options.AddPolicy(ApplicationClaimPolicy.AdminOnly, policy => policy.Requirements.Add(new MyRestaurantAccessRequirement(ApplicationClaimType.Admin)));
+                options.AddPolicy(ApplicationClaimPolicy.ReportOnly, policy => policy.Requirements.Add(new MyRestaurantAccessRequirement(ApplicationClaimType.Report)));
+                options.AddPolicy(ApplicationClaimPolicy.NormalOnly, policy => policy.Requirements.Add(new MyRestaurantAccessRequirement(ApplicationClaimType.Normal)));
             });
         }
         public static void ConfigureAppSettings(this IServiceCollection services, IConfiguration configuration)
         {
-            services.Configure<JWTSettings>(configuration.GetSection("JWTSettings"));
+            services.Configure<JwtSettings>(configuration.GetSection("JWTSettings"));
             services.Configure<SuperAdminAccount>(configuration.GetSection("SuperAdminAccount"));
         }
         public static void ConfigureAuthentication(this IServiceCollection services, IConfiguration configuration)
@@ -57,15 +69,75 @@ namespace MyRestaurant.Api.Extensions
             {
                 options.RequireHttpsMetadata = false; //only for DEV ENV
                 options.SaveToken = false;
+                options.IncludeErrorDetails = true;
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
-                    ValidateIssuerSigningKey = true,
                     ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ClockSkew = TimeSpan.Zero,
                     ValidIssuer = configuration["JWTSettings:Issuer"],
+
+                    ValidateAudience = true,
                     ValidAudience = configuration["JWTSettings:Audience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTSettings:AccessTokenSecret"]))
+
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTSettings:AccessTokenSecret"])),
+
+                    RequireExpirationTime = false,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+                };
+                options.Events = new JwtBearerEvents()
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken))
+                        {
+                            context.Token = accessToken;
+                        }
+                        return Task.CompletedTask;
+                    },
+                    OnChallenge = context =>
+                    {
+                        context.Response.StatusCode = 401;
+                        context.Response.ContentType = "application/json";
+                        var errorCode = HttpStatusCode.Unauthorized;
+                        object error = new 
+                        {
+                            ErrorCode = errorCode,
+                            ErrorType = errorCode.ToString(),
+                            ErrorMessage = string.IsNullOrWhiteSpace(context.Error) ? "Error" : context.Error,
+                            ErrorDate = DateTime.Now
+                        };
+
+                        var result = JsonConvert.SerializeObject(error, Formatting.Indented, new JsonSerializerSettings
+                        {
+                            ContractResolver = new CamelCasePropertyNamesContractResolver()
+                        });
+
+                        context.Response.WriteAsync(result);
+                        return Task.CompletedTask;
+                    },
+                    OnForbidden = context =>
+                    {
+                        context.Response.StatusCode = 403;
+                        context.Response.ContentType = "application/json";
+                        var errorCode = HttpStatusCode.Forbidden;
+                        object error = new
+                        {
+                            ErrorCode = errorCode,
+                            ErrorType = errorCode.ToString(),
+                            ErrorMessage = "You are not authorized to access this resource.",
+                            ErrorDate = DateTime.Now
+                        };
+
+                        var result = JsonConvert.SerializeObject(error, Formatting.Indented, new JsonSerializerSettings
+                        {
+                            ContractResolver = new CamelCasePropertyNamesContractResolver()
+                        });
+                        context.Response.WriteAsync(result);
+                        return Task.CompletedTask;
+                    }
                 };
             });
         }
@@ -120,9 +192,10 @@ namespace MyRestaurant.Api.Extensions
 
         public static void ConfigureServices(this IServiceCollection services)
         {
+            services.AddScoped<IUserAccessor, UserAccessor>();
             services.AddScoped<IServiceTypeService, ServiceTypeService>();
             services.AddScoped<IRestaurantInfoService, RestaurantInfoService>();
-            services.AddScoped<IAccountService, AccountService>();
+            services.AddScoped<IJwtTokenService, JwtTokenService>();
         }
         public static void ConfigureMSSQLContext(this IServiceCollection services, IConfiguration configuration)
         {
