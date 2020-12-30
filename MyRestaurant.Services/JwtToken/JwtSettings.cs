867bc123c26d78ac8581959956438933e6483a96
math2018avan <math2018avan@gmail.com>
2020-12-30 18:46:20 +0800

    token service

---
+++ MyRestaurant.Services/JwtToken/JwtSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyRestaurant.Services
+{
+    public class JwtSettings
+    {
+        public string AccessTokenSecret { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public double AccessTokenExpirationInMinutes { get; set; }
+        public string RefreshTokenSecret { get; set; }
+        public double RefreshTokenExpirationInMinutes { get; set; }
+
+    }
+    
+}
