3ed87c6e579bb4658ab3fabf1b071e73a5185621
math2018avan <math2018avan@gmail.com>
2020-12-30 18:55:56 +0800

    Update AccountController.cs
    account access controller added

--- MyRestaurant.Api/Controllers/V1/Controllers/AccountController.cs
+++ MyRestaurant.Api/Controllers/V1/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyRestaurant.Api.PolicyHandlers;
 using MyRestaurant.Business.Dtos.V1;
 using MyRestaurant.Business.Repositories.Contracts;
 using System.Threading.Tasks;
@@ -16,14 +17,25 @@ namespace MyRestaurant.Api.Controllers.V1.Controllers
             _repository = repository;
         }
 
-        [HttpPost("RegisterAdmin")]
+        [HttpPost("RegisterAdminUser")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status409Conflict)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [AllowAnonymous]
-        public async Task<IActionResult> Register(RegisterDto registerDto)
+        [Authorize(ApplicationClaimPolicy.SuperAdminOnly)]
+        public async Task<IActionResult> RegisterAdmin(RegisterAdminDto registerDto)
         {
             var result = await _repository.RegisterAdminAsync(registerDto);
+            return Ok(result);
+        }
+
+        [HttpPost("RegisterNormalUser")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Authorize(ApplicationClaimPolicy.AdminOnly)]
+        public async Task<IActionResult> RegisterNormalUser(RegisterNormalDto registerDto)
+        {
+            var result = await _repository.RegisterNormalAsync(registerDto);
             return Ok(result);
         }
 
@@ -32,6 +44,7 @@ namespace MyRestaurant.Api.Controllers.V1.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesDefaultResponseType]
+        [AllowAnonymous]
         public async Task<IActionResult> Login(LoginDto loginDto)
         {
             var result = await _repository.LoginAsync(loginDto, IpAddress());
@@ -39,10 +52,27 @@ namespace MyRestaurant.Api.Controllers.V1.Controllers
         }
 
         [HttpPost("Refresh")]
-        public async Task<IActionResult> Refresh(LoginDto loginDto)
+        [AllowAnonymous]
+        public async Task<IActionResult> Refresh(RefreshDto refreshDto)
         {
-            //var result = await _repository.LoginAsync(loginDto);
-            return Ok();
+            var result = await _repository.RefreshToken(refreshDto, IpAddress());
+            return Ok(result);
+        }
+
+        [HttpPost("Revoke")]
+        [Authorize(ApplicationClaimPolicy.AdminOnly)]
+        public async Task<IActionResult> Revoke(RevokeDto revokeDto)
+        {
+            await _repository.RevokeToken(revokeDto, IpAddress());
+            
+            return NoContent();
+        }
+
+        [HttpGet("CurrentUser")]
+        public IActionResult GetCurrentUser()
+        {
+            var currentUser =  _repository.GetCurrentUser();
+            return Ok(currentUser);
         }
 
         private string IpAddress()
