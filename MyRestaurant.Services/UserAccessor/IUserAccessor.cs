8b1c98481e7f4e613a2388cfd9bf0d416ff6f60b
math2018avan <math2018avan@gmail.com>
2020-12-30 18:45:24 +0800

    current User
    current user details from Claims

---
+++ MyRestaurant.Services/UserAccessor/IUserAccessor.cs
@@ -0,0 +1,11 @@
+﻿using MyRestaurant.Models;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Services
+{
+    public interface IUserAccessor
+    {
+        CurrentUser GetCurrentUser();
+    }
+}
---
+++ MyRestaurant.Services/UserAccessor/UserAccessor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+using System.Security.Claims;
+
+namespace MyRestaurant.Services
+{
+    public class UserAccessor : IUserAccessor
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public UserAccessor(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public CurrentUser GetCurrentUser()
+        {
+            var userClaims = _httpContextAccessor.HttpContext.User?.Claims;
+            return new CurrentUser
+            {
+                UserId = new Guid(userClaims?.FirstOrDefault(x => x.Type == "id")?.Value),
+                Email = userClaims?.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value,
+                FirstName = userClaims?.FirstOrDefault(x => x.Type == "firstName")?.Value,
+                LastName = userClaims?.FirstOrDefault(x => x.Type == "lastName")?.Value,
+                Roles = userClaims?.Where(x => x.Type == ClaimTypes.Role).Select(x => x.Value)
+            };
+            
+        }
+    }
+}
