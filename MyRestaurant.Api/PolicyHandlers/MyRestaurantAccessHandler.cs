2753c777190a4d1c320ece34ba59d423fc128523
math2018avan <math2018avan@gmail.com>
2020-12-30 18:55:22 +0800

    Authorization
    Access requirement and handler added

---
+++ MyRestaurant.Api/PolicyHandlers/MyRestaurantAccessHandler.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Api.PolicyHandlers
+{
+    public class MyRestaurantAccessHandler : AuthorizationHandler<MyRestaurantAccessRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, MyRestaurantAccessRequirement requirement)
+        {
+            if (context.User.IsInRole(requirement.Role))
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+    }
+}
