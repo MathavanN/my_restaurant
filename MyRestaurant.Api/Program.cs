82f1ed5201858d4cc38e518455a8a3fb4a1f68df
math2018avan <math2018avan@gmail.com>
2021-01-02 18:21:13 +0800

    Update Program.cs
    serilog added

--- MyRestaurant.Api/Program.cs
+++ MyRestaurant.Api/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using MyRestaurant.Core;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,31 +16,45 @@ namespace MyRestaurant.Api
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
 
-            SeedDatabase(host);
+            try
+            {
+                Log.Information("Application Starting UP.............");
+                var host = CreateHostBuilder(args).Build();
 
-            host.Run();
+                SeedDatabase(host);
+
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application failed to start correctly.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         private static void SeedDatabase(IHost host)
         {
-            using (var scope = host.Services.CreateScope())
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            try
             {
-                var services = scope.ServiceProvider;
-                var logger = services.GetRequiredService<ILogger<Program>>();
-                try
-                {
-                    MyRestaurantSeedData.Initialize(services).Wait();
-                }
-                catch (Exception ex)
-                {
-                    logger.LogError("An error occurred while seeding the database", ex.Message);
-                }
+                MyRestaurantSeedData.Initialize(services).Wait();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("An error occurred while seeding the database", ex.Message);
             }
         }
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
