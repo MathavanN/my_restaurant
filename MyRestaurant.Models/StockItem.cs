de5975a02166ba9c64d483330e9412ba92d7feb8
math2018avan <math2018avan@gmail.com>
2021-01-03 21:18:20 +0800

    new entity
    new models added

---
+++ MyRestaurant.Models/StockItem.cs
@@ -0,0 +1,14 @@
+﻿namespace MyRestaurant.Models
+{
+    public class StockItem : MyRestaurantObject
+    {
+        public long Id { get; set; }
+        public int TypeId { get; set; }
+        public string Name { get; set; }
+        public int UnitOfMeasureId { get; set; }
+        public string Description { get; set; }
+
+        public virtual StockType Type { get; set; }
+        public virtual UnitOfMeasure UnitOfMeasure { get; set; }
+    }
+}
---
+++ MyRestaurant.Models/Supplier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyRestaurant.Models
+{
+    public class Supplier : MyRestaurantObject
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Telephone1 { get; set; }
+        public string Telephone2 { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string ContactPerson { get; set; }
+    }
+}
