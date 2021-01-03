48404bacfead5a82f4a536ad1185d96e7e1000b2
math2018avan <math2018avan@gmail.com>
2021-01-03 21:19:56 +0800

    supplier service
    supplier service

---
+++ MyRestaurant.Services/Supplier/ISupplierService.cs
@@ -0,0 +1,19 @@
+﻿using MyRestaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Services
+{
+    public interface ISupplierService
+    {
+        Task<IEnumerable<Supplier>> GetSuppliersAsync();
+        Task<Supplier> GetSupplierAsync(Expression<Func<Supplier, bool>> expression);
+        Task AddSupplierAsync(Supplier supplier);
+        Task UpdateSupplierAsync(Supplier supplier);
+        Task DeleteSupplierAsync(Supplier supplier);
+    }
+}
---
+++ MyRestaurant.Services/Supplier/SupplierService.cs
@@ -0,0 +1,39 @@
+﻿using MyRestaurant.Core;
+using MyRestaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Services
+{
+    public class SupplierService : ISupplierService
+    {
+        private readonly IMyRestaurantContext _context;
+        public SupplierService(IMyRestaurantContext context)
+        {
+            _context = context;
+        }
+        public async Task AddSupplierAsync(Supplier supplier)
+        {
+            await _context.InsertAsync(supplier);
+            await _context.CommitAsync();
+        }
+
+        public async Task DeleteSupplierAsync(Supplier supplier)
+        {
+            _context.Delete(supplier);
+            await _context.CommitAsync();
+        }
+
+        public async Task<Supplier> GetSupplierAsync(Expression<Func<Supplier, bool>> expression) => await _context.GetFirstOrDefaultAsync(expression);
+
+        public async Task<IEnumerable<Supplier>> GetSuppliersAsync() => await _context.GetAllAsync<Supplier>();
+
+        public async Task UpdateSupplierAsync(Supplier supplier)
+        {
+            _context.Modify(supplier);
+            await _context.CommitAsync();
+        }
+    }
+}
