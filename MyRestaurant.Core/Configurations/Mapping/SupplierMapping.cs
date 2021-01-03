22bee84674dc79cd918af6f084b00aaa9ae08843
math2018avan <math2018avan@gmail.com>
2021-01-03 21:18:54 +0800

    mapping
    new entity mapping added

---
+++ MyRestaurant.Core/Configurations/Mapping/SupplierMapping.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyRestaurant.Core.Configurations.Base;
+using MyRestaurant.Models;
+
+namespace MyRestaurant.Core.Configurations.Mapping
+{
+    internal class SupplierMapping : SupplierMappingBase
+    {
+        public override void Configure(EntityTypeBuilder<Supplier> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Id).UseIdentityColumn();
+            builder.Property(t => t.Name).HasColumnType("varchar(256)").IsRequired();
+            builder.Property(t => t.Address1).HasColumnType("varchar(256)").IsRequired();
+            builder.Property(t => t.Address2);
+            builder.Property(t => t.City).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(t => t.Country).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(t => t.Telephone1).HasColumnType("varchar(20)");
+            builder.Property(t => t.Telephone2).HasColumnType("varchar(20)");
+            builder.Property(t => t.Fax).HasColumnType("varchar(20)");
+            builder.Property(t => t.Email).HasColumnType("varchar(256)");
+            builder.Property(t => t.ContactPerson).HasColumnType("varchar(256)");
+            builder.ToTable("Suppliers");
+
+            base.Configure(builder);
+        }
+    }
+}
---
+++ MyRestaurant.Core/Configurations/Mapping/UnitOfMeasureMapping.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyRestaurant.Core.Configurations.Base;
+using MyRestaurant.Models;
+
+namespace MyRestaurant.Core.Configurations.Mapping
+{
+    internal class UnitOfMeasureMapping : UnitOfMeasureMappingBase
+    {
+        public override void Configure(EntityTypeBuilder<UnitOfMeasure> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Id).UseIdentityColumn();
+            builder.Property(t => t.Code).HasColumnType("varchar(20)").IsRequired();
+            builder.HasIndex(t => t.Code, "IX_UnitOfMeasures").IsUnique();
+            builder.Property(t => t.Description).HasColumnType("varchar(50)");
+            builder.ToTable("UnitOfMeasures");
+
+            base.Configure(builder);
+        }
+    }
+}
