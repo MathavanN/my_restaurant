20d1b4ac371bc80f506a85195801b96fa7f2df38
math2018avan <math2018avan@gmail.com>
2021-01-03 21:19:44 +0800

    uom service
    uom service implementation added

---
+++ MyRestaurant.Services/UnitOfMeasure/IUnitOfMeasureServices.cs
@@ -0,0 +1,17 @@
+﻿using MyRestaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Services
+{
+    public interface IUnitOfMeasureServices
+    {
+        Task<IEnumerable<UnitOfMeasure>> GetUnitOfMeasuresAsync();
+        Task<UnitOfMeasure> GetUnitOfMeasureAsync(Expression<Func<UnitOfMeasure, bool>> expression);
+        Task AddUnitOfMeasureAsync(UnitOfMeasure unitOfMeasure);
+        Task UpdateUnitOfMeasureAsync(UnitOfMeasure unitOfMeasure);
+        Task DeleteUnitOfMeasureAsync(UnitOfMeasure unitOfMeasure);
+    }
+}
