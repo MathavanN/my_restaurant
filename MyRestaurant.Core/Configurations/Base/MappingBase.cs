f5435f2c8e042cea52d5e5fbd21d1044e24e0834
math2018avan <math2018avan@gmail.com>
2021-01-03 21:18:35 +0800

    Update MappingBase.cs
    new entity base mapping

--- MyRestaurant.Core/Configurations/Base/MappingBase.cs
+++ MyRestaurant.Core/Configurations/Base/MappingBase.cs
@@ -6,4 +6,8 @@ namespace MyRestaurant.Core.Configurations.Base
     public abstract class RefreshTokenMappingBase : BaseEntityTypeConfiguration<RefreshToken> { }
     public abstract class ServiceTypeMappingBase : BaseEntityTypeConfiguration<ServiceType> { }
     public abstract class RestaurantInfoMappingBase : BaseEntityTypeConfiguration<RestaurantInfo> { }
+    public abstract class SupplierMappingBase : BaseEntityTypeConfiguration<Supplier> { }
+    public abstract class UnitOfMeasureMappingBase : BaseEntityTypeConfiguration<UnitOfMeasure> { }
+    public abstract class StockTypeMappingBase : BaseEntityTypeConfiguration<StockType> { }
+    public abstract class StockItemMappingBase : BaseEntityTypeConfiguration<StockItem> { }
 }
