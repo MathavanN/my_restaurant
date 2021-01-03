7cddc2eb0e313eef4803b57df0231758199f256e
math2018avan <math2018avan@gmail.com>
2021-01-03 21:20:21 +0800

    stockItem service added

---
+++ MyRestaurant.Services/StockItem/IStockItemServices.cs
@@ -0,0 +1,17 @@
+﻿using MyRestaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Services
+{
+    public interface IStockItemServices
+    {
+        Task<IEnumerable<StockItem>> GetStockItemsAsync();
+        Task<StockItem> GetStockItemAsync(Expression<Func<StockItem, bool>> expression);
+        Task AddStockItemAsync(StockItem stockItem);
+        Task UpdateStockItemAsync(StockItem stockItem);
+        Task DeleteStockItemAsync(StockItem stockItem);
+    }
+}
