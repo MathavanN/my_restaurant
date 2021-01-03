8256e5145893e63f69c0c6430b41d379633b169a
math2018avan <math2018avan@gmail.com>
2021-01-03 21:20:08 +0800

    stocktype service added

---
+++ MyRestaurant.Services/StockType/IStockTypeServices.cs
@@ -0,0 +1,17 @@
+﻿using MyRestaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Services
+{
+    public interface IStockTypeServices
+    {
+        Task<IEnumerable<StockType>> GetStockTypesAsync();
+        Task<StockType> GetStockTypeAsync(Expression<Func<StockType, bool>> expression);
+        Task AddStockTypeAsync(StockType stockType);
+        Task UpdateStockTypeAsync(StockType stockType);
+        Task DeleteStockTypeAsync(StockType stockType);
+    }
+}
