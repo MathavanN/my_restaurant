ecdc925158abc198e0fbd35b68e0bff00a6d46f6
math2018avan <math2018avan@gmail.com>
2021-01-10 19:30:01 +0800

    required validators added

---
+++ MyRestaurant.Api/Validators/V1/PurchaseOrder/CreatePurchaseOrderDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using MyRestaurant.Business.Dtos.V1;
+
+namespace MyRestaurant.Api.Validators.V1
+{
+    public class CreatePurchaseOrderDtoValidator : AbstractValidator<CreatePurchaseOrderDto>
+    {
+        public CreatePurchaseOrderDtoValidator()
+        {
+            RuleFor(x => x.SupplierId).GreaterThan(0).WithMessage("Supplier is required.");
+            RuleFor(x => x.Discount).GreaterThanOrEqualTo(0).WithMessage("Discount must be a positive value.");
+            RuleFor(x => x.Description).MaximumLength(500).WithMessage("Service type maximum length is 20.");
+        }
+    }
+}
