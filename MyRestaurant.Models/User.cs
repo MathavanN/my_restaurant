416b000eea79b1b820e6d8e671304355f9118656
math2018avan <math2018avan@gmail.com>
2021-01-10 19:22:30 +0800

    Update User.cs
    navigation property initialized

--- MyRestaurant.Models/User.cs
+++ MyRestaurant.Models/User.cs
@@ -6,10 +6,19 @@ namespace MyRestaurant.Models
 {
     public class User : IdentityUser<Guid>
     {
+        public User()
+        {
+            RefreshTokens = new HashSet<RefreshToken>();
+            PurchaseOrderRequests = new HashSet<PurchaseOrder>();
+            PurchaseOrderApprovals = new HashSet<PurchaseOrder>();
+        }
+
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
 
         public virtual ICollection<RefreshToken> RefreshTokens { get; set; }
+        public virtual ICollection<PurchaseOrder> PurchaseOrderRequests { get; set; }
+        public virtual ICollection<PurchaseOrder> PurchaseOrderApprovals { get; set; }
     }
 }
