73f5ca0bec9bdb50ca98a19d5a97f852bbbf0a6a
math2018avan <math2018avan@gmail.com>
2021-01-10 19:24:46 +0800

    Update SupplierMapping.cs
    column type added

--- MyRestaurant.Core/Configurations/Mapping/SupplierMapping.cs
+++ MyRestaurant.Core/Configurations/Mapping/SupplierMapping.cs
@@ -13,7 +13,7 @@ namespace MyRestaurant.Core.Configurations.Mapping
             builder.Property(t => t.Id).UseIdentityColumn();
             builder.Property(t => t.Name).HasColumnType("varchar(256)").IsRequired();
             builder.Property(t => t.Address1).HasColumnType("varchar(256)").IsRequired();
-            builder.Property(t => t.Address2);
+            builder.Property(t => t.Address2).HasColumnType("varchar(256)");
             builder.Property(t => t.City).HasColumnType("varchar(100)").IsRequired();
             builder.Property(t => t.Country).HasColumnType("varchar(100)").IsRequired();
             builder.Property(t => t.Telephone1).HasColumnType("varchar(20)");
