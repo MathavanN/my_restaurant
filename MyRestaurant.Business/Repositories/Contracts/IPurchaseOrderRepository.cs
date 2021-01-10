332b0d8f40f31a8313ced8846be9d625c3ee1968
math2018avan <math2018avan@gmail.com>
2021-01-10 19:29:01 +0800

    PO repository added

---
+++ MyRestaurant.Business/Repositories/Contracts/IPurchaseOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using MyRestaurant.Business.Dtos.V1;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Business.Repositories.Contracts
+{
+    public interface IPurchaseOrderRepository
+    {
+        Task<IEnumerable<GetPurchaseOrderDto>> GetPurchaseOrdersAsync();
+        Task<GetPurchaseOrderDto> GetPurchaseOrderAsync(long id);
+        Task<GetPurchaseOrderDto> CreatePurchaseOrderAsync(CreatePurchaseOrderDto purchaseOrderDto);
+    }
+}
