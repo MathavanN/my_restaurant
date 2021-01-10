05690b39be42524482e2cc5420777443ce2c8d06
math2018avan <math2018avan@gmail.com>
2021-01-10 19:21:15 +0800

    Create PurchaseOrderItem.cs
    purchase order item entity adde

---
+++ MyRestaurant.Models/PurchaseOrderItem.cs
@@ -0,0 +1,15 @@
+﻿namespace MyRestaurant.Models
+{
+    public class PurchaseOrderItem : MyRestaurantObject
+    {
+        public long Id { get; set; }
+        public long PurchaseOrderId { get; set; }
+        public long ItemId { get; set; }
+        public decimal ItemUnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public decimal Discount { get; set; }
+
+        public virtual StockItem Item { get; set; }
+        public virtual PurchaseOrder PurchaseOrder { get; set; }
+    }
+}
