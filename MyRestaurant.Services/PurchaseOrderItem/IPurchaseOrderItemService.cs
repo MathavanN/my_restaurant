94e25dd41b7418befbbaf7a7124ccf8eaa5a946e
math2018avan <math2018avan@gmail.com>
2021-01-11 17:22:25 +0800

    PO Item service added

---
+++ MyRestaurant.Services/PurchaseOrderItem/IPurchaseOrderItemService.cs
@@ -0,0 +1,15 @@
+﻿using MyRestaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Services
+{
+    public interface IPurchaseOrderItemService
+    {
+        Task AddPurchaseOrderItemAsync(PurchaseOrderItem orderItem);
+        Task<IEnumerable<PurchaseOrderItem>> GetPurchaseOrderItemsAsync(Expression<Func<PurchaseOrderItem, bool>> expression);
+        Task<PurchaseOrderItem> GetPurchaseOrderItemAsync(Expression<Func<PurchaseOrderItem, bool>> expression);
+    }
+}
