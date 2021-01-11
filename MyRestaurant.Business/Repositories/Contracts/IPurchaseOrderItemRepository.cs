edbc9d75102f8a20cfafc9b5d0f7d06b56fb1a85
math2018avan <math2018avan@gmail.com>
2021-01-11 17:25:13 +0800

    POItemrepo added

---
+++ MyRestaurant.Business/Repositories/Contracts/IPurchaseOrderItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using MyRestaurant.Business.Dtos.V1;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Business.Repositories.Contracts
+{
+    public interface IPurchaseOrderItemRepository
+    {
+        Task<IEnumerable<GetPurchaseOrderItemDto>> GetPurchaseOrderItemsAsync(long orderId);
+        Task<GetPurchaseOrderItemDto> CreatePurchaseOrderItemAsync(CreatePurchaseOrderItemDto purchaseOrderItemDto);
+        Task<GetPurchaseOrderItemDto> GetPurchaseOrderItemAsync(long id);
+    }
+}
