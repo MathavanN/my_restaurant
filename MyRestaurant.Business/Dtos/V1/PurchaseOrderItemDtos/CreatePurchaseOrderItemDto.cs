7ed1e89d432bc1fcf8dfc8fc7a2593e0ed70ec81
math2018avan <math2018avan@gmail.com>
2021-01-11 17:24:36 +0800

    PO ITem related Dtos added

---
+++ MyRestaurant.Business/Dtos/V1/PurchaseOrderItemDtos/CreatePurchaseOrderItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MyRestaurant.Business.Dtos.V1
+{
+    public class CreatePurchaseOrderItemDto
+    {
+        public long PurchaseOrderId { get; set; }
+        public long ItemId { get; set; }
+        public decimal ItemUnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
---
+++ MyRestaurant.Business/Dtos/V1/PurchaseOrderItemDtos/GetPurchaseOrderItemDto.cs
@@ -0,0 +1,15 @@
+﻿namespace MyRestaurant.Business.Dtos.V1
+{
+    public class GetPurchaseOrderItemDto
+    {
+        public long Id { get; set; }
+        public long PurchaseOrderId { get; set; }
+        public long ItemId { get; set; }
+        public string ItemName { get; set; }
+        public decimal ItemUnit { get; set; }
+        public string UnitOfMeasureCode { get; set; }
+        public decimal ItemUnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
