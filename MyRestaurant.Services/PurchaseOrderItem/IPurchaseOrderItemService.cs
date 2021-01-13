9055a6b1238512d6d8c2c521f65000f874d47391
math2018avan <math2018avan@gmail.com>
2021-01-13 20:35:00 +0800

    required PO Item service added

--- MyRestaurant.Services/PurchaseOrderItem/IPurchaseOrderItemService.cs
+++ MyRestaurant.Services/PurchaseOrderItem/IPurchaseOrderItemService.cs
@@ -11,5 +11,7 @@ namespace MyRestaurant.Services
         Task AddPurchaseOrderItemAsync(PurchaseOrderItem orderItem);
         Task<IEnumerable<PurchaseOrderItem>> GetPurchaseOrderItemsAsync(Expression<Func<PurchaseOrderItem, bool>> expression);
         Task<PurchaseOrderItem> GetPurchaseOrderItemAsync(Expression<Func<PurchaseOrderItem, bool>> expression);
+        Task UpdatePurchaseOrderItemAsync(PurchaseOrderItem orderItem);
+        Task DeletePurchaseOrderItemAsync(PurchaseOrderItem orderItem);
     }
 }
--- MyRestaurant.Services/PurchaseOrderItem/PurchaseOrderItemService.cs
+++ MyRestaurant.Services/PurchaseOrderItem/PurchaseOrderItemService.cs
@@ -17,12 +17,23 @@ namespace MyRestaurant.Services
 
         public async Task AddPurchaseOrderItemAsync(PurchaseOrderItem orderItem)
         {
-            await _context.InsertAsync(orderItem);
+            _context.Create(orderItem);
             await _context.CommitAsync();
         }
 
         public async Task<IEnumerable<PurchaseOrderItem>> GetPurchaseOrderItemsAsync(Expression<Func<PurchaseOrderItem, bool>> expression) => await _context.GetAllAsync(expression);
 
         public async Task<PurchaseOrderItem> GetPurchaseOrderItemAsync(Expression<Func<PurchaseOrderItem, bool>> expression) => await _context.GetFirstOrDefaultAsync(expression);
+
+        public async Task UpdatePurchaseOrderItemAsync(PurchaseOrderItem orderItem)
+        {
+            _context.Modify(orderItem);
+            await _context.CommitAsync();
+        }
+        public async Task DeletePurchaseOrderItemAsync(PurchaseOrderItem orderItem)
+        {
+            _context.Delete(orderItem);
+            await _context.CommitAsync();
+        }
     }
 }
