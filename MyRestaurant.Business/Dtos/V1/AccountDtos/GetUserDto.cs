fa301a231576f9403a0151c8a8a532a5d989dfff
math2018avan <math2018avan@gmail.com>
2021-01-23 16:49:50 +0800

    Create GetUserDto.cs
    user dto added

---
+++ MyRestaurant.Business/Dtos/V1/AccountDtos/GetUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyRestaurant.Business.Dtos.V1
+{
+    public class GetUserDto
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
