06d99086df3362c6feb33d8369e5d7176ad436fc
math2018avan <math2018avan@gmail.com>
2021-01-23 16:57:16 +0800

    new method added

--- MyRestaurant.Services/StockItem/IStockItemServices.cs
+++ MyRestaurant.Services/StockItem/IStockItemServices.cs
@@ -1,6 +1,7 @@
 ﻿using MyRestaurant.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace MyRestaurant.Services
     public interface IStockItemServices
     {
         Task<IEnumerable<StockItem>> GetStockItemsAsync();
+        IQueryable<StockItem> GetStockItemsAsync(Expression<Func<StockItem, bool>> expression);
         Task<StockItem> GetStockItemAsync(Expression<Func<StockItem, bool>> expression);
         Task AddStockItemAsync(StockItem stockItem);
         Task UpdateStockItemAsync(StockItem stockItem);
