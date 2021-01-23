a181c1e54a3f99982fc616c0eb9c581b7d98f3ea
math2018avan <math2018avan@gmail.com>
2021-01-23 16:52:50 +0800

    user role mapping added

---
+++ MyRestaurant.Core/Migrations/20210117031649_IdentityRoleMapping.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyRestaurant.Core.Migrations
+{
+    public partial class IdentityRoleMapping : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
