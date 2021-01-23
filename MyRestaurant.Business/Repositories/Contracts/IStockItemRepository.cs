dcc309f25fbdda18b708e403788ad8390f15ec86
math2018avan <math2018avan@gmail.com>
2021-01-23 16:58:54 +0800

    filter params added

--- MyRestaurant.Business/Repositories/Contracts/IStockItemRepository.cs
+++ MyRestaurant.Business/Repositories/Contracts/IStockItemRepository.cs
@@ -7,6 +7,7 @@ namespace MyRestaurant.Business.Repositories.Contracts
     public interface IStockItemRepository
     {
         Task<IEnumerable<GetStockItemDto>> GetStockItemsAsync();
+        Task<StockItemEnvelop> GetStockItemByType(int typeId, int? limit, int? offset);
         Task<GetStockItemDto> GetStockItemAsync(long id);
         Task<GetStockItemDto> CreateStockItemAsync(CreateStockItemDto stockItemDto);
         Task UpdateStockItemAsync(long id, EditStockItemDto stockItemDto);
