1fbd141c7ebcf1b2f976c953783d5e12c44a41aa
math2018avan <math2018avan@gmail.com>
2021-01-30 20:51:04 +0800

    grn service added

---
+++ MyRestaurant.Services/GoodsReceivedNote/IGoodsReceivedNoteService.cs
@@ -0,0 +1,17 @@
+﻿using MyRestaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Services
+{
+    public interface IGoodsReceivedNoteService
+    {
+        Task<IEnumerable<GoodsReceivedNote>> GetGoodsReceivedNotesAsync();
+        Task<GoodsReceivedNote> GetGoodsReceivedNoteAsync(Expression<Func<GoodsReceivedNote, bool>> expression);
+        Task AddGoodsReceivedNoteAsync(GoodsReceivedNote goodsReceivedNote);
+        Task UpdateGoodsReceivedNoteAsync(GoodsReceivedNote goodsReceivedNote);
+        Task DeleteGoodsReceivedNoteAsync(GoodsReceivedNote goodsReceivedNote);
+    }
+}
