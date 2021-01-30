704067d0ce9095e29b2fef599ba2b3495e773a66
math2018avan <math2018avan@gmail.com>
2021-01-30 20:51:51 +0800

    payment type service added

---
+++ MyRestaurant.Services/PayemntType/PaymentTypeService.cs
@@ -0,0 +1,40 @@
+﻿using MyRestaurant.Core;
+using MyRestaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Services
+{
+    public class PaymentTypeService : IPaymentTypeService
+    {
+        private readonly IMyRestaurantContext _context;
+        public PaymentTypeService(IMyRestaurantContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddPaymentTypeAsync(PaymentType paymentType)
+        {
+            _context.Create(paymentType);
+            await _context.CommitAsync();
+        }
+
+        public async Task DeletePaymentTypeAsync(PaymentType paymentType)
+        {
+            _context.Delete(paymentType);
+            await _context.CommitAsync();
+        }
+
+        public async Task<PaymentType> GetPaymentTypeAsync(Expression<Func<PaymentType, bool>> expression) => await _context.GetFirstOrDefaultAsync(expression);
+
+        public async Task<IEnumerable<PaymentType>> GetPaymentTypesAsync() => await _context.GetAllAsync<PaymentType>();
+
+        public async Task UpdatePaymentTypeAsync(PaymentType paymentType)
+        {
+            _context.Modify(paymentType);
+            await _context.CommitAsync();
+        }
+    }
+}
