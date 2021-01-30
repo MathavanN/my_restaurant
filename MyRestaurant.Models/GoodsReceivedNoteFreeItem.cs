15c45d5f22173e08ea7b40cfadee33cb25584af9
math2018avan <math2018avan@gmail.com>
2021-01-30 20:47:54 +0800

    Create GoodsReceivedNoteFreeItem.cs
    GRN freeItem model added

---
+++ MyRestaurant.Models/GoodsReceivedNoteFreeItem.cs
@@ -0,0 +1,17 @@
+﻿namespace MyRestaurant.Models
+{
+    public class GoodsReceivedNoteFreeItem : MyRestaurantObject
+    {
+        public long Id { get; set; }
+        public long GoodsReceivedNoteId { get; set; }
+        public long ItemId { get; set; }
+        public decimal ItemUnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public decimal Nbt { get; set; }
+        public decimal Vat { get; set; }
+        public decimal Discount { get; set; }
+
+        public virtual GoodsReceivedNote GoodsReceivedNote { get; set; }
+        public virtual StockItem Item { get; set; }
+    }
+}
