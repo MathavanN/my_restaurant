30b348e974b43aef5e99bcc52667ec8de75567ce
math2018avan <math2018avan@gmail.com>
2021-01-30 20:55:17 +0800

    discount validation removed

--- MyRestaurant.Api/Validators/V1/PurchaseOrderItem/CreatePurchaseOrderItemDtoValidator.cs
+++ MyRestaurant.Api/Validators/V1/PurchaseOrderItem/CreatePurchaseOrderItemDtoValidator.cs
@@ -9,7 +9,6 @@ namespace MyRestaurant.Api.Validators.V1
         {
             RuleFor(x => x.PurchaseOrderId).GreaterThan(0).WithMessage("Purchase order id is required.");
             RuleFor(x => x.ItemId).GreaterThan(0).WithMessage("Item type is required.");
-            RuleFor(x => x.Discount).GreaterThanOrEqualTo(0).WithMessage("Discount must be a positive value.");
             RuleFor(x => x.Quantity).GreaterThan(0).WithMessage("Quantity must be grater than zero.");
             RuleFor(x => x.ItemUnitPrice).GreaterThan(0).WithMessage("Item unit price must be greater than zero.");
         }
