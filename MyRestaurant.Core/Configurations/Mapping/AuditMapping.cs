63b4da16aafcd1c5001b11f405071c2d22295c3b
math2018avan <math2018avan@gmail.com>
2021-01-30 20:50:03 +0800

    Update AuditMapping.cs
    changes added

--- MyRestaurant.Core/Configurations/Mapping/AuditMapping.cs
+++ MyRestaurant.Core/Configurations/Mapping/AuditMapping.cs
@@ -9,10 +9,10 @@ namespace MyRestaurant.Core.Configurations.Mapping
     {
         public override void Configure(EntityTypeBuilder<Audit> builder)
         {
-            builder.HasKey(t => t.Id);
-            builder.Property(t => t.Action).HasColumnType("varchar(20)").IsRequired().HasMaxLength(20);
-            builder.Property(t => t.TableName).HasColumnType("varchar(256)").IsRequired();
-            builder.Property(t => t.Username).HasColumnType("varchar(256)").IsRequired();
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Action).HasColumnType("varchar(20)").IsRequired().HasMaxLength(20);
+            builder.Property(e => e.TableName).HasColumnType("varchar(256)").IsRequired();
+            builder.Property(e => e.Username).HasColumnType("varchar(256)").IsRequired();
             builder.ToTable("Audits");
             base.Configure(builder);
         }
