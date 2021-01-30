1d3ad20371e663f95ac2f9e1a32e9e5fb214e9b8
math2018avan <math2018avan@gmail.com>
2021-01-30 20:50:16 +0800

    Create PaymentTypeMapping.cs
    payment type mapping

---
+++ MyRestaurant.Core/Configurations/Mapping/PaymentTypeMapping.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyRestaurant.Core.Configurations.Base;
+using MyRestaurant.Models;
+
+namespace MyRestaurant.Core.Configurations.Mapping
+{
+    internal class PaymentTypeMapping : PaymentTypeMappingBase
+    {
+        public override void Configure(EntityTypeBuilder<PaymentType> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Id).UseIdentityColumn();
+            builder.Property(e => e.Name).HasColumnType("varchar(30)").IsRequired();
+            builder.HasIndex(e => e.Name, "IX_PaymentTypes").IsUnique();
+            builder.Property(e => e.CreditPeriod);
+            builder.ToTable("PaymentTypes");
+
+            base.Configure(builder);
+        }
+    }
+}
