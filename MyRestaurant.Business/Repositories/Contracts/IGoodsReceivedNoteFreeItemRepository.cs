df5f12aa0a61afa78c2ac30cfc2b46e9a2db616e
math2018avan <math2018avan@gmail.com>
2021-01-30 20:53:44 +0800

    grn free item repo added

---
+++ MyRestaurant.Business/Repositories/Contracts/IGoodsReceivedNoteFreeItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using MyRestaurant.Business.Dtos.V1;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Business.Repositories.Contracts
+{
+    public interface IGoodsReceivedNoteFreeItemRepository
+    {
+        Task<IEnumerable<GetGoodsReceivedNoteFreeItemDto>> GetGoodsReceivedNoteFreeItemsAsync(long orderId);
+        Task<GetGoodsReceivedNoteFreeItemDto> CreateGoodsReceivedNoteFreeItemAsync(CreateGoodsReceivedNoteFreeItemDto goodsReceivedNoteFreeItemDto);
+        Task<GetGoodsReceivedNoteFreeItemDto> GetGoodsReceivedNoteFreeItemAsync(long id);
+        Task UpdateGoodsReceivedNoteFreeItemAsync(long id, EditGoodsReceivedNoteFreeItemDto goodsReceivedNoteFreeItemDto);
+        Task DeleteGoodsReceivedNoteFreeItemAsync(long id);
+    }
+}
