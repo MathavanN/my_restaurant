985747876d6d943fd835f2d672d13f713a13c328
math2018avan <math2018avan@gmail.com>
2021-01-30 20:53:58 +0800

    grn item repo added

---
+++ MyRestaurant.Business/Repositories/GoodsReceivedNoteItemRepository.cs
@@ -0,0 +1,77 @@
+﻿using AutoMapper;
+using MyRestaurant.Business.Dtos.V1;
+using MyRestaurant.Business.Errors;
+using MyRestaurant.Business.Repositories.Contracts;
+using MyRestaurant.Models;
+using MyRestaurant.Services;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Business.Repositories
+{
+    public class GoodsReceivedNoteItemRepository : IGoodsReceivedNoteItemRepository
+    {
+        private readonly IMapper _mapper;
+        private readonly IGoodsReceivedNoteItemSevice _goodsReceivedNoteItem;
+        public GoodsReceivedNoteItemRepository(IMapper mapper, IGoodsReceivedNoteItemSevice goodsReceivedNoteItem)
+        {
+            _mapper = mapper;
+            _goodsReceivedNoteItem = goodsReceivedNoteItem;
+        }
+
+        private async Task CheckGoodsReceivedNoteItemAsync(long id, long goodsReceivedNoteId, long itemId)
+        {
+            var dbItem = await _goodsReceivedNoteItem.GetGoodsReceivedNoteItemAsync(d => d.ItemId == itemId && d.GoodsReceivedNoteId == goodsReceivedNoteId && d.Id != id);
+            if (dbItem != null)
+                throw new RestException(HttpStatusCode.Conflict, $"Item already available for this goods received note.");
+        }
+        private async Task<GoodsReceivedNoteItem> GetGoodsReceivedNoteItemById(long id)
+        {
+            var item = await _goodsReceivedNoteItem.GetGoodsReceivedNoteItemAsync(d => d.Id == id);
+
+            if (item == null)
+                throw new RestException(HttpStatusCode.NotFound, "Goods received note item not found.");
+
+            return item;
+        }
+
+        public async Task<GetGoodsReceivedNoteItemDto> CreateGoodsReceivedNoteItemAsync(CreateGoodsReceivedNoteItemDto goodsReceivedNoteItemDto)
+        {
+            await CheckGoodsReceivedNoteItemAsync(0, goodsReceivedNoteItemDto.GoodsReceivedNoteId, goodsReceivedNoteItemDto.ItemId);
+
+            var item = _mapper.Map<GoodsReceivedNoteItem>(goodsReceivedNoteItemDto);
+            await _goodsReceivedNoteItem.AddGoodsReceivedNoteItemAsync(item);
+
+            return _mapper.Map<GetGoodsReceivedNoteItemDto>(item);
+        }
+
+        public async Task DeleteGoodsReceivedNoteItemAsync(long id)
+        {
+            var item = await GetGoodsReceivedNoteItemById(id);
+
+            await _goodsReceivedNoteItem.DeleteGoodsReceivedNoteItemAsync(item);
+        }
+
+        public async Task<GetGoodsReceivedNoteItemDto> GetGoodsReceivedNoteItemAsync(long id)
+        {
+            var item = await GetGoodsReceivedNoteItemById(id);
+
+            return _mapper.Map<GetGoodsReceivedNoteItemDto>(item);
+        }
+
+        public async Task<IEnumerable<GetGoodsReceivedNoteItemDto>> GetGoodsReceivedNoteItemsAsync(long goodsReceivedNoteId)
+        {
+            var items = await _goodsReceivedNoteItem.GetGoodsReceivedNoteItemsAsync(d => d.GoodsReceivedNoteId == goodsReceivedNoteId);
+
+            return _mapper.Map<IEnumerable<GetGoodsReceivedNoteItemDto>>(items);
+        }
+
+        public async Task UpdateGoodsReceivedNoteItemAsync(long id, EditGoodsReceivedNoteItemDto goodsReceivedNoteItemDto)
+        {
+            var item = await GetGoodsReceivedNoteItemById(id);
+
+            await _goodsReceivedNoteItem.DeleteGoodsReceivedNoteItemAsync(item);
+        }
+    }
+}
