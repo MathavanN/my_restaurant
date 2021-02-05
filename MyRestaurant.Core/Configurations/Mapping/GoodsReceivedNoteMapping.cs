3298efdfd99a1f2fe1c8c8425422427055212e81
math2018avan <math2018avan@gmail.com>
2021-02-05 17:56:14 +0800

    Update GoodsReceivedNoteMapping.cs
    model mapping added

--- MyRestaurant.Core/Configurations/Mapping/GoodsReceivedNoteMapping.cs
+++ MyRestaurant.Core/Configurations/Mapping/GoodsReceivedNoteMapping.cs
@@ -17,23 +17,27 @@ namespace MyRestaurant.Core.Configurations.Mapping
             builder.Property(e => e.Nbt).HasColumnType("decimal(18, 2)");
             builder.Property(e => e.Vat).HasColumnType("decimal(18, 2)");
             builder.Property(e => e.Discount).HasColumnType("decimal(18, 2)");
-            builder.Property(e => e.ReceivedDate).HasColumnType("datetime").IsRequired();
             builder.Property(e => e.ReceivedBy).IsRequired();
-            builder.Property(e => e.CreatedDate).HasColumnType("datetime");
-            builder.Property(e => e.CreatedBy);
+            builder.Property(e => e.ReceivedDate).HasColumnType("datetime").IsRequired();
+            builder.Property(e => e.CreatedBy).IsRequired();
+            builder.Property(e => e.CreatedDate).HasColumnType("datetime").IsRequired();
+            builder.Property(e => e.ApprovalStatus).IsRequired();
+            builder.Property(e => e.ApprovedBy);
+            builder.Property(e => e.ApprovedDate).HasColumnType("datetime");
+            builder.Property(e => e.ApprovalReason).HasColumnType("varchar(500)");
             builder.ToTable("GoodsReceivedNotes");
 
             builder.HasOne(d => d.ReceivedUser)
-                .WithMany(p => p.GoodsReceivedNotes)
+                .WithMany(p => p.GRNReceives)
                 .HasForeignKey(d => d.ReceivedBy)
                 .OnDelete(DeleteBehavior.ClientCascade)
                 .HasConstraintName("FK_GoodsReceivedNotes_ReceivedBy");
 
-            builder.HasOne(d => d.CreatedUser)
-                .WithMany(p => p.GoodsCreatedNotes)
-                .HasForeignKey(d => d.CreatedBy)
+            builder.HasOne(d => d.ApprovedUser)
+                .WithMany(p => p.GRNApprovals)
+                .HasForeignKey(d => d.ApprovedBy)
                 .OnDelete(DeleteBehavior.ClientCascade)
-                .HasConstraintName("FK_GoodsReceivedNotes_CreatedBy");
+                .HasConstraintName("FK_GoodsReceivedNotes_ApprovedBy");
 
             builder.HasOne(d => d.PaymentType)
                 .WithMany(p => p.GoodsReceivedNotes)
@@ -42,10 +46,10 @@ namespace MyRestaurant.Core.Configurations.Mapping
                 .HasConstraintName("FK_GoodsReceivedNotes_PaymentTypes");
 
             builder.HasOne(d => d.PurchaseOrder)
-                .WithOne(p => p.GoodsReceivedNote)
-                .HasForeignKey<GoodsReceivedNote>(d => d.PurchaseOrderId)
+                .WithMany(p => p.GoodsReceivedNotes)
+                .HasForeignKey(d => d.PurchaseOrderId)
                 .OnDelete(DeleteBehavior.ClientCascade)
-                .HasConstraintName("FK_GoodsReceivedNotes_PurchaseOrder");
+                .HasConstraintName("FK_GoodsReceivedNotes_PurchaseOrders");
 
             base.Configure(builder);
         }
