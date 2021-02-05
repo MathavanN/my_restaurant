879ea9d50d46492a5ce2853d397a08dd59098801
math2018avan <math2018avan@gmail.com>
2021-02-05 17:55:52 +0800

    Update GoodsReceivedNote.cs
    new props added

--- MyRestaurant.Models/GoodsReceivedNote.cs
+++ MyRestaurant.Models/GoodsReceivedNote.cs
@@ -18,13 +18,17 @@ namespace MyRestaurant.Models
         public decimal Nbt { get; set; }
         public decimal Vat { get; set; }
         public decimal Discount { get; set; }
-        public DateTime ReceivedDate { get; set; }
         public Guid ReceivedBy { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public DateTime ReceivedDate { get; set; }
+        public Guid? ApprovedBy { get; set; }
+        public DateTime? ApprovedDate { get; set; }
+        public Status ApprovalStatus { get; set; }
+        public string ApprovalReason { get; set; }
         public Guid CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
 
         public virtual User ReceivedUser { get; set; }
-        public virtual User CreatedUser { get; set; }
+        public virtual User ApprovedUser { get; set; }
         public virtual PaymentType PaymentType { get; set; }
         public virtual PurchaseOrder PurchaseOrder { get; set; }
         public virtual ICollection<GoodsReceivedNoteFreeItem> GoodsReceivedNoteFreeItems { get; set; }
