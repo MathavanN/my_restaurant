bebcd7d4142c5fee23deed1bb3784f908b13a641
math2018avan <math2018avan@gmail.com>
2021-02-11 14:45:07 +0800

    simple test for controller added

---
+++ MyRestaurant.Api.Tests/Controllers/V2/AboutControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using MyRestaurant.Api.Controllers.V2;
+using Xunit;
+
+namespace MyRestaurant.Api.Tests.Controllers.V2
+{
+    public class AboutControllerTest
+    {
+        [Fact]
+        public void Get_ReturnOkObjectResult()
+        {
+            //Arrange
+            var controller = new AboutController();
+
+            //Act
+            var result = controller.Get();
+
+            //Assert
+            var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+            okResult.StatusCode.Should().Be(200);
+            okResult.Value.Should().BeEquivalentTo(new { data = "This is About Details v2" });
+        }
+    }
+}
