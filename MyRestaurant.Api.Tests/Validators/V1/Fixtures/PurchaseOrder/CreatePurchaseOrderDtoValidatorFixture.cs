b629837ad4def9b6ed3feec140ff796f241114db
math2018avan <math2018avan@gmail.com>
2021-02-12 13:01:02 +0800

    fixtures added

---
+++ MyRestaurant.Api.Tests/Validators/V1/Fixtures/PurchaseOrder/CreatePurchaseOrderDtoValidatorFixture.cs
@@ -0,0 +1,28 @@
+﻿using MyRestaurant.Api.Validators.V1;
+using MyRestaurant.Business.Dtos.V1;
+using System;
+
+namespace MyRestaurant.Api.Tests.Validators.V1.Fixtures
+{
+    public class CreatePurchaseOrderDtoValidatorFixture : IDisposable
+    {
+        public CreatePurchaseOrderDto Model { get; set; }
+        public CreatePurchaseOrderDtoValidator Validator { get; private set; }
+
+        public CreatePurchaseOrderDtoValidatorFixture()
+        {
+            Validator = new CreatePurchaseOrderDtoValidator();
+
+            Model = new CreatePurchaseOrderDto
+            {
+                SupplierId = 1,
+                Description = "Purchase order description"
+            };
+        }
+        public void Dispose()
+        {
+            Model = null;
+            Validator = null;
+        }
+    }
+}
