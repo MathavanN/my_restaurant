1df556d58986eaf308ea6e78383da76238a032ec
math2018avan <math2018avan@gmail.com>
2021-02-13 19:18:10 +0800

    Update RegisterNormalDtoValidator.cs
    validation changed

--- MyRestaurant.Api/Validators/V1/Account/RegisterNormalDtoValidator.cs
+++ MyRestaurant.Api/Validators/V1/Account/RegisterNormalDtoValidator.cs
@@ -12,9 +12,12 @@ namespace MyRestaurant.Api.Validators.V1
 
             RuleFor(x => x.LastName).NotEmpty().WithMessage("LastName is required.");
 
-            RuleFor(x => x.Email).EmailAddress().WithMessage("Email is not a valid email address.");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required.")
+                                 .NotNull().WithMessage("Email must not be empty.")
+                                 .EmailAddress().WithMessage("Email is not a valid email address.");
 
-            RuleFor(x => x.Password).NotEmpty().Equal(x => x.ConfirmPassword).WithMessage("Password must be same.");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required.")
+                                    .Equal(x => x.ConfirmPassword).WithMessage("Password must be same.");
 
             RuleFor(x => x.Roles).NotEmpty().WithMessage("Role is required.");
             RuleForEach(x => x.Roles).IsEnumName(typeof(NormalUserRoles), caseSensitive: false);
