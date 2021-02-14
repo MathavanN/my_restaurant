02ad4d1d09d33e0531c21feff175701f8964a9ec
math2018avan <math2018avan@gmail.com>
2021-02-14 16:39:04 +0800

    Update IPurchaseOrderItemService.cs
    Add new item return that item

--- MyRestaurant.Services/PurchaseOrderItem/IPurchaseOrderItemService.cs
+++ MyRestaurant.Services/PurchaseOrderItem/IPurchaseOrderItemService.cs
@@ -8,7 +8,7 @@ namespace MyRestaurant.Services
 {
     public interface IPurchaseOrderItemService
     {
-        Task AddPurchaseOrderItemAsync(PurchaseOrderItem orderItem);
+        Task<PurchaseOrderItem> AddPurchaseOrderItemAsync(PurchaseOrderItem orderItem);
         Task<IEnumerable<PurchaseOrderItem>> GetPurchaseOrderItemsAsync(Expression<Func<PurchaseOrderItem, bool>> expression);
         Task<PurchaseOrderItem> GetPurchaseOrderItemAsync(Expression<Func<PurchaseOrderItem, bool>> expression);
         Task UpdatePurchaseOrderItemAsync(PurchaseOrderItem orderItem);
