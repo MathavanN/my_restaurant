b3e852bdbd5041433843cb157d4f3d7bb5cc1e67
math2018avan <math2018avan@gmail.com>
2021-02-14 16:38:12 +0800

    Update IStockItemServices.cs
    Add new item return the item with navigation properties

--- MyRestaurant.Services/StockItem/IStockItemServices.cs
+++ MyRestaurant.Services/StockItem/IStockItemServices.cs
@@ -12,7 +12,7 @@ namespace MyRestaurant.Services
         Task<IEnumerable<StockItem>> GetStockItemsAsync();
         IQueryable<StockItem> GetStockItemsAsync(Expression<Func<StockItem, bool>> expression);
         Task<StockItem> GetStockItemAsync(Expression<Func<StockItem, bool>> expression);
-        Task AddStockItemAsync(StockItem stockItem);
+        Task<StockItem> AddStockItemAsync(StockItem stockItem);
         Task UpdateStockItemAsync(StockItem stockItem);
         Task DeleteStockItemAsync(StockItem stockItem);
     }
