e681212a783179a2ca9c0adbcae212f0886f55e7
math2018avan <math2018avan@gmail.com>
2021-02-19 16:29:36 +0800

    Create AutoMapperSingleton.cs
    Autommapper added

---
+++ MyRestaurant.Business.Tests/AutoMapperSingleton.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+
+namespace MyRestaurant.Business.Tests
+{
+    class AutoMapperSingleton
+    {
+        private static IMapper _mapper;
+        public static IMapper Mapper
+        {
+            get
+            {
+                if (_mapper == null)
+                {
+                    var configuration = new MapperConfiguration(cfg =>
+                    {
+                        cfg.AddProfile(new AutoMapping.AutoMapping());
+                    });
+                    _mapper = configuration.CreateMapper();
+                }
+                return _mapper;
+            }
+        }
+    }
+}
