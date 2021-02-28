11eb73c590dc2eb756fc7d9fbf1f99d227c3d910
math2018avan <math2018avan@gmail.com>
2021-02-28 15:57:46 +0800

    grn item repo test cases added

--- MyRestaurant.Business.Tests/Repositories/Fixtures/GoodsReceivedNoteItemRepositoryFixture.cs
+++ MyRestaurant.Business.Tests/Repositories/Fixtures/GoodsReceivedNoteItemRepositoryFixture.cs
@@ -1,15 +1,116 @@
 ﻿using Moq;
+using MyRestaurant.Business.Dtos.V1;
+using MyRestaurant.Models;
 using MyRestaurant.Services;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MyRestaurant.Business.Tests.Repositories.Fixtures
 {
     public class GoodsReceivedNoteItemRepositoryFixture : IDisposable
     {
         public Mock<IGoodsReceivedNoteItemService> MockGoodsReceivedNoteItemService { get; private set; }
-
+        public IEnumerable<GoodsReceivedNoteItem> GoodsReceivedNoteItems { get; private set; }
+        public CreateGoodsReceivedNoteItemDto CreateGoodsReceivedNoteItemDto { get; private set; }
+        public GoodsReceivedNoteItem CreatedNewGoodsReceivedNoteItem { get; private set; }
+        public EditGoodsReceivedNoteItemDto EditGoodsReceivedNoteItemDto { get; private set; }
         public GoodsReceivedNoteItemRepositoryFixture()
         {
+            MockGoodsReceivedNoteItemService = new Mock<IGoodsReceivedNoteItemService>();
+
+            var unitOfMeasure1 = new UnitOfMeasure { Id = 1, Code = "kg", Description = "" };
+            var unitOfMeasure2 = new UnitOfMeasure { Id = 2, Code = "g", Description = "" };
+            var unitOfMeasure3 = new UnitOfMeasure { Id = 3, Code = "l", Description = "" };
+            var unitOfMeasure4 = new UnitOfMeasure { Id = 4, Code = "ml", Description = "" };
+            var unitOfMeasure5 = new UnitOfMeasure { Id = 5, Code = "none", Description = "" };
+
+            var stockType1 = new StockType { Id = 1, Type = "Grocery" };
+            var stockType2 = new StockType { Id = 2, Type = "Beverage" };
+            var stockType3 = new StockType { Id = 3, Type = "Stationery" };
+
+            var stockItems = new List<StockItem>
+            {
+                new StockItem { Id = 20025, TypeId = 1, Type = stockType1, Name = "Rice", ItemUnit = 10, UnitOfMeasureId = 1, UnitOfMeasure = unitOfMeasure1 },
+                new StockItem { Id = 20026, TypeId = 1, Type = stockType1, Name = "Chilli Powder", ItemUnit = 250, UnitOfMeasureId = 2, UnitOfMeasure = unitOfMeasure2 },
+                new StockItem { Id = 20050, TypeId = 2, Type = stockType2, Name = "Water", ItemUnit = 1, UnitOfMeasureId = 3, UnitOfMeasure = unitOfMeasure3 },
+                new StockItem { Id = 20024, TypeId = 3, Type = stockType3, Name = "Blue Pen", ItemUnit = 1, UnitOfMeasureId = 5, UnitOfMeasure = unitOfMeasure5 },
+                new StockItem { Id = 20023, TypeId = 5, Type = stockType1, Name = "Rice", ItemUnit = 10, UnitOfMeasureId = 1, UnitOfMeasure = unitOfMeasure1 },
+            };
+
+            GoodsReceivedNoteItems = new List<GoodsReceivedNoteItem>
+            {
+                new GoodsReceivedNoteItem {
+                    Id = 1,
+                    GoodsReceivedNoteId = 101,
+                    Item = stockItems.FirstOrDefault(d => d.Id == 20025),
+                    ItemId = 20025,
+                    ItemUnitPrice = 540,
+                    Quantity = 5,
+                    Nbt = 0.1m,
+                    Vat = 0.1m,
+                    Discount = 0.1m
+                },
+                new GoodsReceivedNoteItem {
+                    Id = 2,
+                    GoodsReceivedNoteId = 202,
+                    Item = stockItems.FirstOrDefault(d => d.Id == 20026),
+                    ItemId = 20026,
+                    ItemUnitPrice = 30,
+                    Quantity = 10,
+                    Nbt = 0.1m,
+                    Vat = 0.1m,
+                    Discount = 0.1m
+                },
+                new GoodsReceivedNoteItem {
+                    Id = 3,
+                    GoodsReceivedNoteId = 101,
+                    Item = stockItems.FirstOrDefault(d => d.Id == 20050),
+                    ItemId = 20050,
+                    ItemUnitPrice = 50,
+                    Quantity = 5,
+                    Nbt = 0.1m,
+                    Vat = 0.1m,
+                    Discount = 0.1m
+                },
+                new GoodsReceivedNoteItem {
+                    Id = 4,
+                    GoodsReceivedNoteId = 202,
+                    Item = stockItems.FirstOrDefault(d => d.Id == 20024),
+                    ItemId = 20024,
+                    ItemUnitPrice = 260,
+                    Quantity = 6,
+                    Nbt = 0.1m,
+                    Vat = 0.1m,
+                    Discount = 0.1m
+                }
+            };
+
+            CreateGoodsReceivedNoteItemDto = new CreateGoodsReceivedNoteItemDto
+            {
+                GoodsReceivedNoteId = 202,
+                ItemId = 20023,
+                ItemUnitPrice = 350,
+                Quantity = 5
+            };
+
+            CreatedNewGoodsReceivedNoteItem = new GoodsReceivedNoteItem
+            {
+                Id = 5,
+                GoodsReceivedNoteId = 202,
+                Item = stockItems.FirstOrDefault(d => d.Id == 20023),
+                ItemId = 20023,
+                ItemUnitPrice = 350,
+                Quantity = 5
+            };
+
+            EditGoodsReceivedNoteItemDto = new EditGoodsReceivedNoteItemDto
+            {
+                GoodsReceivedNoteId = 101,
+                ItemId = 20025,
+                ItemUnitPrice = 650,
+                Quantity = 7
+            };
         }
         public void Dispose()
         {
---
+++ MyRestaurant.Business.Tests/Repositories/GoodsReceivedNoteItemRepositoryTest.cs
@@ -0,0 +1,242 @@
+﻿using FluentAssertions;
+using Moq;
+using MyRestaurant.Business.Dtos.V1;
+using MyRestaurant.Business.Errors;
+using MyRestaurant.Business.Repositories;
+using MyRestaurant.Business.Tests.Repositories.Fixtures;
+using MyRestaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+namespace MyRestaurant.Business.Tests.Repositories
+{
+    public class GoodsReceivedNoteItemRepositoryTest : IClassFixture<GoodsReceivedNoteItemRepositoryFixture>
+    {
+        private readonly GoodsReceivedNoteItemRepositoryFixture _fixture;
+        public GoodsReceivedNoteItemRepositoryTest(GoodsReceivedNoteItemRepositoryFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async void GetGoodsReceivedNoteItemsAsync_Returns_GetGoodsReceivedNoteItemDtos()
+        {
+            //Arrange
+            _fixture.MockGoodsReceivedNoteItemService.Setup(x => x.GetGoodsReceivedNoteItemsAsync(It.IsAny<Expression<Func<GoodsReceivedNoteItem, bool>>>()))
+                .Returns<Expression<Func<GoodsReceivedNoteItem, bool>>>(async (expression) =>
+                {
+                    var orders = _fixture.GoodsReceivedNoteItems.AsQueryable().Where(expression).ToList();
+                    return await Task.FromResult(orders);
+                });
+
+            var repository = new GoodsReceivedNoteItemRepository(AutoMapperSingleton.Mapper, _fixture.MockGoodsReceivedNoteItemService.Object);
+
+            //Act
+            var result = await repository.GetGoodsReceivedNoteItemsAsync(101);
+
+            //Assert
+            var orders = result.Should().BeAssignableTo<IEnumerable<GetGoodsReceivedNoteItemDto>>().Subject;
+            orders.Should().HaveCount(2);
+        }
+
+        [Fact]
+        public async void GetGoodsReceivedNoteItemAsync_Returns_GetGoodsReceivedNoteItemDto()
+        {
+            //Arrange
+            var id = 1;
+            _fixture.MockGoodsReceivedNoteItemService.Setup(x => x.GetGoodsReceivedNoteItemAsync(It.IsAny<Expression<Func<GoodsReceivedNoteItem, bool>>>()))
+                .Returns<Expression<Func<GoodsReceivedNoteItem, bool>>>(expression => Task.FromResult(_fixture.GoodsReceivedNoteItems.AsQueryable().FirstOrDefault(expression)));
+
+            var repository = new GoodsReceivedNoteItemRepository(AutoMapperSingleton.Mapper, _fixture.MockGoodsReceivedNoteItemService.Object);
+
+            //Act
+            var result = await repository.GetGoodsReceivedNoteItemAsync(id);
+
+            //Assert
+            result.Should().BeOfType(typeof(GetGoodsReceivedNoteItemDto));
+            result.Id.Should().Be(id);
+            result.ItemTypeName.Should().Be("Grocery");
+            result.ItemName.Should().Be("Rice");
+        }
+
+        [Fact]
+        public async void GetGoodsReceivedNoteItemAsync_Returns_NotFoundException()
+        {
+            //Arrange
+            var id = 201;
+            _fixture.MockGoodsReceivedNoteItemService.Setup(x => x.GetGoodsReceivedNoteItemAsync(It.IsAny<Expression<Func<GoodsReceivedNoteItem, bool>>>()))
+                .Returns<Expression<Func<GoodsReceivedNoteItem, bool>>>(expression => Task.FromResult(_fixture.GoodsReceivedNoteItems.AsQueryable().FirstOrDefault(expression)));
+
+            var repository = new GoodsReceivedNoteItemRepository(AutoMapperSingleton.Mapper, _fixture.MockGoodsReceivedNoteItemService.Object);
+
+            //Act
+            var exception = await Assert.ThrowsAsync<RestException>(() => repository.GetGoodsReceivedNoteItemAsync(id));
+
+            //Assert
+            exception.ErrorCode.Should().Be(HttpStatusCode.NotFound);
+            exception.ErrorMessage.Should().Be("Goods received note item not found.");
+            exception.ErrorType.Should().Be(HttpStatusCode.NotFound.ToString());
+        }
+
+        [Fact]
+        public async void CreateGoodsReceivedNoteItemAsync_Returns_New_GetGoodsReceivedNoteItemDto()
+        {
+            //Arrange
+            _fixture.MockGoodsReceivedNoteItemService.Setup(x => x.AddGoodsReceivedNoteItemAsync(It.IsAny<GoodsReceivedNoteItem>()))
+                .ReturnsAsync(_fixture.CreatedNewGoodsReceivedNoteItem);
+
+            var repository = new GoodsReceivedNoteItemRepository(AutoMapperSingleton.Mapper, _fixture.MockGoodsReceivedNoteItemService.Object);
+
+            //Act
+            var result = await repository.CreateGoodsReceivedNoteItemAsync(_fixture.CreateGoodsReceivedNoteItemDto);
+
+            //Assert
+            result.Should().BeOfType(typeof(GetGoodsReceivedNoteItemDto));
+            result.Id.Should().Be(5);
+            result.ItemTypeName.Should().Be("Grocery");
+            result.ItemName.Should().Be("Rice");
+            result.ItemUnitPrice.Should().Be(350);
+            result.Quantity.Should().Be(5);
+        }
+
+        [Fact]
+        public async void CreateGoodsReceivedNoteItemAsync_Returns_ConflictException()
+        {
+            //Arrange
+            _fixture.MockGoodsReceivedNoteItemService.Setup(x => x.GetGoodsReceivedNoteItemAsync(It.IsAny<Expression<Func<GoodsReceivedNoteItem, bool>>>()))
+                .Returns<Expression<Func<GoodsReceivedNoteItem, bool>>>(expression => Task.FromResult(_fixture.GoodsReceivedNoteItems.AsQueryable().FirstOrDefault(expression)));
+
+            var repository = new GoodsReceivedNoteItemRepository(AutoMapperSingleton.Mapper, _fixture.MockGoodsReceivedNoteItemService.Object);
+
+            //Act
+            var exception = await Assert.ThrowsAsync<RestException>(() => repository.CreateGoodsReceivedNoteItemAsync(new CreateGoodsReceivedNoteItemDto
+            {
+                GoodsReceivedNoteId = 202,
+                ItemId = 20024,
+                ItemUnitPrice = 350,
+                Quantity = 5
+            }));
+
+            //Assert
+            exception.ErrorCode.Should().Be(HttpStatusCode.Conflict);
+            exception.ErrorMessage.Should().Be("Item already available for this goods received note.");
+            exception.ErrorType.Should().Be(HttpStatusCode.Conflict.ToString());
+        }
+
+        [Fact]
+        public async void UpdateGoodsReceivedNoteItemAsync_Returns_Updated_GetGoodsReceivedNoteItemDto()
+        {
+            //Arrange
+            var id = 1;
+            _fixture.MockGoodsReceivedNoteItemService.Setup(x => x.GetGoodsReceivedNoteItemAsync(It.IsAny<Expression<Func<GoodsReceivedNoteItem, bool>>>()))
+                .Returns<Expression<Func<GoodsReceivedNoteItem, bool>>>(expression => Task.FromResult(_fixture.GoodsReceivedNoteItems.AsQueryable().FirstOrDefault(expression)));
+
+            _fixture.MockGoodsReceivedNoteItemService.Setup(x => x.UpdateGoodsReceivedNoteItemAsync(It.IsAny<GoodsReceivedNoteItem>()));
+
+            var repository = new GoodsReceivedNoteItemRepository(AutoMapperSingleton.Mapper, _fixture.MockGoodsReceivedNoteItemService.Object);
+
+            //Act
+            var result = await repository.UpdateGoodsReceivedNoteItemAsync(id, _fixture.EditGoodsReceivedNoteItemDto);
+
+            //Assert
+            result.Should().BeOfType(typeof(GetGoodsReceivedNoteItemDto));
+            result.Id.Should().Be(id);
+            result.ItemTypeName.Should().Be("Grocery");
+            result.ItemName.Should().Be("Rice");
+            result.ItemUnitPrice.Should().Be(650);
+            result.Quantity.Should().Be(7);
+        }
+
+        [Fact]
+        public async void UpdateGoodsReceivedNoteItemAsync_Returns_NotFoundException()
+        {
+            //Arrange
+            var id = 201;
+            _fixture.MockGoodsReceivedNoteItemService.Setup(x => x.GetGoodsReceivedNoteItemAsync(It.IsAny<Expression<Func<GoodsReceivedNoteItem, bool>>>()))
+                .Returns<Expression<Func<GoodsReceivedNoteItem, bool>>>(expression => Task.FromResult(_fixture.GoodsReceivedNoteItems.AsQueryable().FirstOrDefault(expression)));
+
+            _fixture.MockGoodsReceivedNoteItemService.Setup(x => x.UpdateGoodsReceivedNoteItemAsync(It.IsAny<GoodsReceivedNoteItem>()));
+
+            var repository = new GoodsReceivedNoteItemRepository(AutoMapperSingleton.Mapper, _fixture.MockGoodsReceivedNoteItemService.Object);
+
+            //Act
+            var exception = await Assert.ThrowsAsync<RestException>(() => repository.UpdateGoodsReceivedNoteItemAsync(id, _fixture.EditGoodsReceivedNoteItemDto));
+
+            //Assert
+            exception.ErrorCode.Should().Be(HttpStatusCode.NotFound);
+            exception.ErrorMessage.Should().Be("Goods received note item not found.");
+            exception.ErrorType.Should().Be(HttpStatusCode.NotFound.ToString());
+        }
+
+        [Fact]
+        public async void UpdateGoodsReceivedNoteItemAsync_Returns_ConflictException()
+        {
+            //Arrange
+            var id = 2;
+            _fixture.MockGoodsReceivedNoteItemService.Setup(x => x.GetGoodsReceivedNoteItemAsync(It.IsAny<Expression<Func<GoodsReceivedNoteItem, bool>>>()))
+                .Returns<Expression<Func<GoodsReceivedNoteItem, bool>>>(expression => Task.FromResult(_fixture.GoodsReceivedNoteItems.AsQueryable().FirstOrDefault(expression)));
+
+            _fixture.MockGoodsReceivedNoteItemService.Setup(x => x.UpdateGoodsReceivedNoteItemAsync(It.IsAny<GoodsReceivedNoteItem>()));
+
+            var repository = new GoodsReceivedNoteItemRepository(AutoMapperSingleton.Mapper, _fixture.MockGoodsReceivedNoteItemService.Object);
+
+            //Act
+            var exception = await Assert.ThrowsAsync<RestException>(() => repository.UpdateGoodsReceivedNoteItemAsync(id, new EditGoodsReceivedNoteItemDto
+            {
+                GoodsReceivedNoteId = 202,
+                ItemId = 20024,
+                ItemUnitPrice = 260,
+                Quantity = 6
+            }));
+
+            //Assert
+            exception.ErrorCode.Should().Be(HttpStatusCode.Conflict);
+            exception.ErrorMessage.Should().Be("Item already available for this goods received note.");
+            exception.ErrorType.Should().Be(HttpStatusCode.Conflict.ToString());
+        }
+
+        [Fact]
+        public async void DeleteGoodsReceivedNoteItemAsync_Returns_NoResult()
+        {
+            //Arrange
+            var id = 2;
+            _fixture.MockGoodsReceivedNoteItemService.Setup(x => x.GetGoodsReceivedNoteItemAsync(It.IsAny<Expression<Func<GoodsReceivedNoteItem, bool>>>()))
+                .Returns<Expression<Func<GoodsReceivedNoteItem, bool>>>(expression => Task.FromResult(_fixture.GoodsReceivedNoteItems.AsQueryable().FirstOrDefault(expression)));
+
+            _fixture.MockGoodsReceivedNoteItemService.Setup(x => x.DeleteGoodsReceivedNoteItemAsync(It.IsAny<GoodsReceivedNoteItem>()));
+
+            var repository = new GoodsReceivedNoteItemRepository(AutoMapperSingleton.Mapper, _fixture.MockGoodsReceivedNoteItemService.Object);
+
+            //Act
+            await repository.DeleteGoodsReceivedNoteItemAsync(id);
+
+            // Assert
+            _fixture.MockGoodsReceivedNoteItemService.Verify(x => x.DeleteGoodsReceivedNoteItemAsync(It.IsAny<GoodsReceivedNoteItem>()), Times.Once);
+        }
+
+        [Fact]
+        public async void DeleteGoodsReceivedNoteItemAsync_Returns_NotFoundException()
+        {
+            //Arrange
+            var id = 201;
+            _fixture.MockGoodsReceivedNoteItemService.Setup(x => x.GetGoodsReceivedNoteItemAsync(It.IsAny<Expression<Func<GoodsReceivedNoteItem, bool>>>()))
+                .Returns<Expression<Func<GoodsReceivedNoteItem, bool>>>(expression => Task.FromResult(_fixture.GoodsReceivedNoteItems.AsQueryable().FirstOrDefault(expression)));
+
+            _fixture.MockGoodsReceivedNoteItemService.Setup(x => x.DeleteGoodsReceivedNoteItemAsync(It.IsAny<GoodsReceivedNoteItem>()));
+
+            var repository = new GoodsReceivedNoteItemRepository(AutoMapperSingleton.Mapper, _fixture.MockGoodsReceivedNoteItemService.Object);
+
+            //Act
+            var exception = await Assert.ThrowsAsync<RestException>(() => repository.DeleteGoodsReceivedNoteItemAsync(id));
+
+            //Assert
+            exception.ErrorCode.Should().Be(HttpStatusCode.NotFound);
+            exception.ErrorMessage.Should().Be("Goods received note item not found.");
+            exception.ErrorType.Should().Be(HttpStatusCode.NotFound.ToString());
+        }
+    }
+}
