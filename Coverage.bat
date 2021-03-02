fec437ea08f42037b124d64140123630a4b00a9d
math2018avan <math2018avan@gmail.com>
2021-03-02 17:02:06 +0800

    Update Coverage.bat
    new test project coverage added

--- Coverage.bat
+++ Coverage.bat
@@ -4,6 +4,8 @@ dotnet test MyRestaurant.Api.Tests/MyRestaurant.Api.Tests.csproj --logger "trx;L
 
 dotnet test MyRestaurant.Business.Tests/MyRestaurant.Business.Tests.csproj --logger "trx;LogFileName=MyRestaurant.Business.Tests.Results.trx" --logger "xunit;LogFileName=MyRestaurant.Business.Tests.Results.xml" --results-directory ./CoverageReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=%CD%/CoverageReports/Coverage/MyRestaurant.Business.Tests.Results.coverage.cobertura.xml /p:CoverletOutputFormat=cobertura /p:Exclude="[xunit.*]"
 
+dotnet test MyRestaurant.Services.Tests/MyRestaurant.Services.Tests.csproj --logger "trx;LogFileName=MyRestaurant.Services.Tests.Results.trx" --logger "xunit;LogFileName=MyRestaurant.Services.Tests.Results.xml" --results-directory ./CoverageReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=%CD%/CoverageReports/Coverage/MyRestaurant.Services.Tests.Results.coverage.cobertura.xml /p:CoverletOutputFormat=cobertura /p:Exclude="[xunit.*]"
+
 %UserProfile%\.nuget\packages\reportgenerator\4.8.5\tools\net5.0\ReportGenerator.exe "-reports:%CD%\CoverageReports\Coverage\*.cobertura.xml" "-targetdir:CoverageReports\Coverage" -reporttypes:HTML
 
 start %CD%\CoverageReports\Coverage\index.html
