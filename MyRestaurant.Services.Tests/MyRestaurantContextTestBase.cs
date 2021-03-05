ba7a97641fbab68c6d118b69908e81a92528e56f
math2018avan <math2018avan@gmail.com>
2021-03-05 20:21:54 +0800

    Create MyRestaurantContextTestBase.cs
    in memory db initialization

---
+++ MyRestaurant.Services.Tests/MyRestaurantContextTestBase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRestaurant.Core;
+using System;
+
+namespace MyRestaurant.Services.Tests
+{
+    public class MyRestaurantContextTestBase : IDisposable
+    {
+        protected readonly MyRestaurantContext _myRestaurantContext;
+
+        public MyRestaurantContextTestBase()
+        {
+            var options = new DbContextOptionsBuilder<MyRestaurantContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _myRestaurantContext = new MyRestaurantContext(options);
+
+            _myRestaurantContext.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            _myRestaurantContext.Database.EnsureDeleted();
+
+            _myRestaurantContext.Dispose();
+        }
+    }
+}
