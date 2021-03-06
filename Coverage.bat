ccc473d43ab1c9eafe2fe917dd32f7a7a85cf91b
math2018avan <math2018avan@gmail.com>
2021-03-06 19:45:24 +0800

    Update Coverage.bat
    test order changed

--- Coverage.bat
+++ Coverage.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
 
-dotnet test MyRestaurant.Api.Tests/MyRestaurant.Api.Tests.csproj --logger "trx;LogFileName=MyRestaurant.Api.Test.Results.trx" --logger "xunit;LogFileName=MyRestaurant.Api.Test.Results.xml" --results-directory ./CoverageReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=%CD%/CoverageReports/Coverage/MyRestaurant.Api.Test.Results.coverage.cobertura.xml /p:CoverletOutputFormat=cobertura /p:Exclude="[xunit.*]"
+dotnet test MyRestaurant.Services.Tests/MyRestaurant.Services.Tests.csproj --logger "xunit;LogFileName=MyRestaurant.Services.Tests.Results.xml" --results-directory ./CoverageReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=%CD%/CoverageReports/Coverage/MyRestaurant.Services.Tests.Results.coverage.cobertura.xml /p:CoverletOutputFormat=cobertura /p:Exclude="[xunit.*]"
 
-dotnet test MyRestaurant.Business.Tests/MyRestaurant.Business.Tests.csproj --logger "trx;LogFileName=MyRestaurant.Business.Tests.Results.trx" --logger "xunit;LogFileName=MyRestaurant.Business.Tests.Results.xml" --results-directory ./CoverageReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=%CD%/CoverageReports/Coverage/MyRestaurant.Business.Tests.Results.coverage.cobertura.xml /p:CoverletOutputFormat=cobertura /p:Exclude="[xunit.*]"
+dotnet test MyRestaurant.Business.Tests/MyRestaurant.Business.Tests.csproj --logger "xunit;LogFileName=MyRestaurant.Business.Tests.Results.xml" --results-directory ./CoverageReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=%CD%/CoverageReports/Coverage/MyRestaurant.Business.Tests.Results.coverage.cobertura.xml /p:CoverletOutputFormat=cobertura /p:Exclude="[xunit.*]"
 
-dotnet test MyRestaurant.Services.Tests/MyRestaurant.Services.Tests.csproj --logger "trx;LogFileName=MyRestaurant.Services.Tests.Results.trx" --logger "xunit;LogFileName=MyRestaurant.Services.Tests.Results.xml" --results-directory ./CoverageReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=%CD%/CoverageReports/Coverage/MyRestaurant.Services.Tests.Results.coverage.cobertura.xml /p:CoverletOutputFormat=cobertura /p:Exclude="[xunit.*]"
+dotnet test MyRestaurant.Api.Tests/MyRestaurant.Api.Tests.csproj --logger "xunit;LogFileName=MyRestaurant.Api.Test.Results.xml" --results-directory ./CoverageReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=%CD%/CoverageReports/Coverage/MyRestaurant.Api.Test.Results.coverage.cobertura.xml /p:CoverletOutputFormat=cobertura /p:Exclude="[xunit.*]"
 
 %UserProfile%\.nuget\packages\reportgenerator\4.8.5\tools\net5.0\ReportGenerator.exe "-reports:%CD%\CoverageReports\Coverage\*.cobertura.xml" "-targetdir:CoverageReports\Coverage" -reporttypes:HTML
 
