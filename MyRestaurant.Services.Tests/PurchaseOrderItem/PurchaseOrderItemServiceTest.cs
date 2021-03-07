8cae00a298c88c991c0361ae2d208c18ef342a15
math2018avan <math2018avan@gmail.com>
2021-03-07 16:44:26 +0800

    Update PurchaseOrderItemServiceTest.cs
    cleanup

--- MyRestaurant.Services.Tests/PurchaseOrderItem/PurchaseOrderItemServiceTest.cs
+++ MyRestaurant.Services.Tests/PurchaseOrderItem/PurchaseOrderItemServiceTest.cs
@@ -7,6 +7,7 @@ namespace MyRestaurant.Services.Tests
 {
     public class PurchaseOrderItemServiceTest : MyRestaurantContextTestBase
     {
+
         public PurchaseOrderItemServiceTest()
         {
             PurchaseOrderItemInitializer.Initialize(_myRestaurantContext);
@@ -22,8 +23,8 @@ namespace MyRestaurant.Services.Tests
             var result = await service.GetPurchaseOrderItemsAsync(d => d.PurchaseOrderId == 1);
 
             //Assert
-            var uoms = result.Should().BeAssignableTo<IEnumerable<PurchaseOrderItem>>().Subject;
-            uoms.Should().HaveCount(2);
+            result.Should().BeAssignableTo<IEnumerable<PurchaseOrderItem>>();
+            result.Should().HaveCount(2);
         }
 
         [Fact]
@@ -37,12 +38,13 @@ namespace MyRestaurant.Services.Tests
             var result = await service.GetPurchaseOrderItemAsync(d => d.Id == id);
 
             //Assert
-            var item = result.Should().BeAssignableTo<PurchaseOrderItem>().Subject;
-            item.Id.Should().Be(id);
-            item.Item.Name.Should().Be("Rice");
-            item.PurchaseOrder.OrderNumber.Should().Be("PO_20210130_8d8c510caee6a4b");
-            item.ItemUnitPrice.Should().Be(540);
-            item.Quantity.Should().Be(5);
+            result.Should().BeAssignableTo<PurchaseOrderItem>();
+            result.PurchaseOrder.Should().BeAssignableTo<PurchaseOrder>();
+            result.Id.Should().Be(id);
+            result.Item.Name.Should().Be("Rice");
+            result.PurchaseOrder.OrderNumber.Should().Be("PO_20210130_8d8c510caee6a4b");
+            result.ItemUnitPrice.Should().Be(540);
+            result.Quantity.Should().Be(5);
         }
 
         [Fact]
@@ -74,11 +76,11 @@ namespace MyRestaurant.Services.Tests
             });
 
             //Assert
-            var item = result.Should().BeAssignableTo<PurchaseOrderItem>().Subject;
-            item.Item.Name.Should().Be("Pasta");
-            item.PurchaseOrder.OrderNumber.Should().Be("PO_20210130_8d8c512f7cd7920");
-            item.ItemUnitPrice.Should().Be(350);
-            item.Quantity.Should().Be(5);
+            result.Should().BeAssignableTo<PurchaseOrderItem>();
+            result.Item.Name.Should().Be("Pasta");
+            result.PurchaseOrder.OrderNumber.Should().Be("PO_20210130_8d8c512f7cd7920");
+            result.ItemUnitPrice.Should().Be(350);
+            result.Quantity.Should().Be(5);
 
             //Act
             var items = await service.GetPurchaseOrderItemsAsync(d => d.PurchaseOrderId == 2);
@@ -106,12 +108,12 @@ namespace MyRestaurant.Services.Tests
             var result = await service.GetPurchaseOrderItemAsync(d => d.Id == id);
 
             //Assert
-            var item = result.Should().BeAssignableTo<PurchaseOrderItem>().Subject;
-            item.Id.Should().Be(id);
-            item.Item.Name.Should().Be("Sausages");
-            item.PurchaseOrder.OrderNumber.Should().Be("PO_20210130_8d8c512f7cd7920");
-            item.ItemUnitPrice.Should().Be(450);
-            item.Quantity.Should().Be(3);
+            result.Should().BeAssignableTo<PurchaseOrderItem>();
+            result.Id.Should().Be(id);
+            result.Item.Name.Should().Be("Sausages");
+            result.PurchaseOrder.OrderNumber.Should().Be("PO_20210130_8d8c512f7cd7920");
+            result.ItemUnitPrice.Should().Be(450);
+            result.Quantity.Should().Be(3);
         }
 
         [Fact]
