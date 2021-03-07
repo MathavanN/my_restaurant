a9ff8cea8b8838ec5a0d1e88f9fd3ba859901fb4
math2018avan <math2018avan@gmail.com>
2021-03-07 16:46:41 +0800

    Create UserAccessorServiceTest.cs
    user accessor service test cases added

---
+++ MyRestaurant.Services.Tests/UserAccessor/UserAccessorServiceTest.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Xunit;
+
+namespace MyRestaurant.Services.Tests
+{
+    public class UserAccessorServiceTest
+    {
+        [Fact]
+        public void GetCurrentUser_Returns_CurrentUser()
+        {
+            //Arrange
+            var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            var claims = new List<Claim>
+            {
+                new Claim ("id", Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Email, "abc@gmail.com"),
+                new Claim("firstName", "Normal"),
+                new Claim(ClaimTypes.Role, "Normal"),
+                new Claim("lastName", "Access"),
+            };
+            mockHttpContextAccessor.Setup(d => d.HttpContext.User.Claims).Returns(claims);
+            var service = new UserAccessorService(mockHttpContextAccessor.Object);
+
+            //Act
+            var result = service.GetCurrentUser();
+
+            //Assert
+            result.Should().NotBeNull().And.BeOfType<CurrentUser>();
+        }
+
+        [Fact]
+        public void GetCurrentUser_Throws_ArgumentNullException()
+        {
+            //Arrange
+            var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            ClaimsPrincipal claimsPrincipal = null;
+            mockHttpContextAccessor.Setup(d => d.HttpContext.User).Returns(claimsPrincipal);
+            var service = new UserAccessorService(mockHttpContextAccessor.Object);
+
+            //Act
+            var exception = Assert.Throws<ArgumentNullException>(() => service.GetCurrentUser());
+
+            //Assert
+            exception.Message.Should().Contain("Value cannot be null.");
+        }
+    }
+}
