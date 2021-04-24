6c9508bfb457ae2302ceaceda592c49f1e9b482b
math2018avan <math2018avan@gmail.com>
2021-04-24 18:48:40 +0800

    Create TransactionTypeMapping.cs
    transaction type mapping added

---
+++ MyRestaurant.Core/Configurations/Mapping/TransactionTypeMapping.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyRestaurant.Core.Configurations.Base;
+using MyRestaurant.Models;
+
+namespace MyRestaurant.Core.Configurations.Mapping
+{
+    internal class TransactionTypeMapping : TransactionTypeMappingBase
+    {
+        public override void Configure(EntityTypeBuilder<TransactionType> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Id).UseIdentityColumn();
+            builder.Property(e => e.Type).HasColumnType("varchar(50)").IsRequired().HasMaxLength(50);
+            builder.ToTable("TransactionTypes");
+            base.Configure(builder);
+        }
+    }
+}
