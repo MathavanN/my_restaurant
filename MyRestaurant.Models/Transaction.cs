ff7a05dcff6ea5fe0c95490db6c427878b7b3dcc
math2018avan <math2018avan@gmail.com>
2021-04-24 18:46:45 +0800

    Create Transaction.cs
    transaction model added

---
+++ MyRestaurant.Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyRestaurant.Models
+{
+    public class Transaction : MyRestaurantObject
+    {
+        public long Id { get; set; }
+        public int TransactionTypeId { get; set; }
+        public int PaymentTypeId { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+        public Cashflow Cashflow { get; set; }
+
+        public virtual PaymentType PaymentType { get; set; }
+        public virtual TransactionType TransactionType { get; set; }
+    }
+
+    public enum Cashflow
+    {
+        Income = 0,
+        Expense = 1,
+    }
+}
