53cecb6debafdb8f428adf3404124dbb2990cde8
math2018avan <math2018avan@gmail.com>
2021-04-24 18:52:00 +0800

    Transaction service added
    Transaction service added

---
+++ MyRestaurant.Services/Transaction/TransactionService.cs
@@ -0,0 +1,41 @@
+﻿using MyRestaurant.Core;
+using MyRestaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MyRestaurant.Services
+{
+    public class TransactionService : ITransactionService
+    {
+        private readonly IMyRestaurantContext _context;
+        public TransactionService(IMyRestaurantContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Transaction> AddTransactionAsync(Transaction transaction)
+        {
+            _context.Create(transaction);
+            await _context.CommitAsync();
+            return transaction;
+        }
+
+        public async Task DeleteTransactionAsync(Transaction transaction)
+        {
+            _context.Delete(transaction);
+            await _context.CommitAsync();
+        }
+
+        public async Task<Transaction> GetTransactionAsync(Expression<Func<Transaction, bool>> expression) => await _context.GetFirstOrDefaultAsync(expression);
+
+        public async Task<IEnumerable<Transaction>> GetTransactionsAsync() => await _context.GetAllAsync<Transaction>();
+
+        public async Task UpdateTransactionAsync(Transaction transaction)
+        {
+            _context.Modify(transaction);
+            await _context.CommitAsync();
+        }
+    }
+}
