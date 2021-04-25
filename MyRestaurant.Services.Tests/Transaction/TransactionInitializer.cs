2f7e40a14726bbe51489875331a28de41ab8dab7
math2018avan <math2018avan@gmail.com>
2021-04-25 13:52:00 +0800

    TransactionService test cases added

---
+++ MyRestaurant.Services.Tests/Transaction/TransactionInitializer.cs
@@ -0,0 +1,46 @@
+﻿using MyRestaurant.Core;
+using MyRestaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyRestaurant.Services.Tests
+{
+    public class TransactionInitializer
+    {
+        public static void Initialize(MyRestaurantContext context)
+        {
+            TransactionTypeInitializer.Initialize(context);
+
+            PaymentTypeInitializer.Initialize(context);
+
+            if (!context.Transactions.Any())
+            {
+                var transactions = new List<Transaction>
+                {
+                    new Transaction {
+                        TransactionTypeId = 1,
+                        PaymentTypeId = 2,
+                        Date = DateTime.Now.AddDays(-10),
+                        Description = "Peanuts in Coke",
+                        Amount = 6.5m,
+                        Cashflow = Cashflow.Expense,
+                        CreatedAt = DateTime.Now
+                    },
+                    new Transaction {
+                        TransactionTypeId = 10,
+                        PaymentTypeId = 1,
+                        Date = DateTime.Now.AddDays(-5),
+                        Description = "Income from sale",
+                        Amount = 110.5m,
+                        Cashflow = Cashflow.Income,
+                        CreatedAt = DateTime.Now
+                    }
+                };
+
+                context.Transactions.AddRange(transactions);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ MyRestaurant.Services.Tests/Transaction/TransactionServiceTest.cs
@@ -0,0 +1,132 @@
+﻿using FluentAssertions;
+using MyRestaurant.Models;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MyRestaurant.Services.Tests
+{
+    public class TransactionServiceTest : MyRestaurantContextTestBase
+    {
+        public TransactionServiceTest()
+        {
+            TransactionInitializer.Initialize(_myRestaurantContext);
+        }
+
+        [Fact]
+        public async void GetTransactionsAsync_Returns_Transactions()
+        {
+            //Arrange
+            var service = new TransactionService(_myRestaurantContext);
+
+            //Act
+            var result = await service.GetTransactionsAsync();
+
+            //Assert
+            result.Should().BeAssignableTo<IEnumerable<Transaction>>();
+            result.Should().HaveCount(2);
+        }
+
+        [Fact]
+        public async void GetTransactionAsync_Returns_Transaction()
+        {
+            //Arrange
+            var id = 1;
+            var service = new TransactionService(_myRestaurantContext);
+
+            //Act
+            var result = await service.GetTransactionAsync(d => d.Id == id);
+
+            //Assert
+            result.Should().BeAssignableTo<Transaction>();
+            result.Id.Should().Be(id);
+            result.TransactionTypeId.Should().Be(1);
+            result.PaymentTypeId.Should().Be(2);
+        }
+
+        [Fact]
+        public async void GetTransactionTypeAsync_Returns_Null()
+        {
+            //Arrange
+            var id = 1001;
+            var service = new TransactionService(_myRestaurantContext);
+
+            //Act
+            var result = await service.GetTransactionAsync(d => d.Id == id);
+
+            //Assert
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public async void AddTransactionTypeAsync_Returns_New_TransactionType()
+        {
+            //Arrange
+            var service = new TransactionService(_myRestaurantContext);
+
+            //Act
+            var result = await service.AddTransactionAsync(new Transaction {
+                TransactionTypeId = 10,
+                PaymentTypeId = 1,
+                Date = DateTime.Now.AddDays(-2),
+                Description = "Interest from Deposit",
+                Amount = 456.5m,
+                Cashflow = Cashflow.Income,
+                CreatedAt = DateTime.Now
+            });
+
+            //Assert
+            result.Should().BeAssignableTo<Transaction>();
+            result.TransactionTypeId.Should().Be(10);
+            result.TransactionType.Type.Should().Be("Extra Income");
+            result.PaymentTypeId.Should().Be(1);
+            result.PaymentType.Name.Should().Be("Cash");
+            result.Cashflow.Should().Be(Cashflow.Income);
+
+            //Act
+            var transactionTypes = await service.GetTransactionsAsync();
+
+            //Assert
+            transactionTypes.Should().HaveCount(3);
+        }
+
+        [Fact]
+        public async void UpdateTransactionTypeAsync_Successfully_Updated()
+        {
+            //Arrange
+            var id = 2;
+            var service = new TransactionService(_myRestaurantContext);
+
+            //Act
+            var dbTransaction = await service.GetTransactionAsync(d => d.Id == id);
+            dbTransaction.Amount = 567.5m;
+
+            await service.UpdateTransactionAsync(dbTransaction);
+
+            var result = await service.GetTransactionAsync(d => d.Id == id);
+
+            //Assert
+            result.Should().BeAssignableTo<Transaction>();
+            result.Id.Should().Be(id);
+            result.Amount.Should().Be(567.5m);
+        }
+
+        [Fact]
+        public async void DeleteTransactionTypeAsync_Successfully_Deleted()
+        {
+            //Arrange
+            var id = 1;
+            var service = new TransactionService(_myRestaurantContext);
+
+            //Act
+            var dbTransactionType = await service.GetTransactionAsync(d => d.Id == id);
+
+            await service.DeleteTransactionAsync(dbTransactionType);
+
+            var result = await service.GetTransactionAsync(d => d.Id == id);
+
+            //Assert
+            result.Should().BeNull();
+        }
+    }
+}
