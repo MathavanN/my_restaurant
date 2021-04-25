2f27881f5c7449a41283d8909dc15275e6793244
math2018avan <math2018avan@gmail.com>
2021-04-25 14:06:34 +0800

    Transaction related dtos added

---
+++ MyRestaurant.Business/Dtos/V1/TransactionDtos/CreateTransactionDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyRestaurant.Business.Dtos.V1
+{
+    public class CreateTransactionDto
+    {
+        public int TransactionTypeId { get; set; }
+        public int PaymentTypeId { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+        public string Cashflow { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ MyRestaurant.Business/Dtos/V1/TransactionDtos/GetTransactionDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyRestaurant.Business.Dtos.V1
+{
+    public class GetTransactionDto
+    {
+        public long Id { get; set; }
+        public int TransactionTypeId { get; set; }
+        public string TransactionType { get; set; }
+        public int PaymentTypeId { get; set; }
+        public string PaymentType { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+        public string Cashflow { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
