9d3e70bd9d0aefedfc48b51da36df6fc9d12d1d1
math2018avan <math2018avan@gmail.com>
2021-04-25 17:19:57 +0800

    test case added

---
+++ MyRestaurant.Business.Tests/Repositories/TransactionRepositoryTest.cs
@@ -0,0 +1,187 @@
+﻿using FluentAssertions;
+using Moq;
+using MyRestaurant.Business.Dtos.V1;
+using MyRestaurant.Business.Errors;
+using MyRestaurant.Business.Repositories;
+using MyRestaurant.Business.Tests.Repositories.Fixtures;
+using MyRestaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyRestaurant.Business.Tests.Repositories
+{
+    public class TransactionRepositoryTest : IClassFixture<TransactionRepositoryFixture>
+    {
+        private readonly TransactionRepositoryFixture _fixture;
+        public TransactionRepositoryTest(TransactionRepositoryFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async void GetTransactionsAsync_Returns_GetTransactionDtos()
+        {
+            //Arrange
+            _fixture.MockTransactionService.Setup(x => x.GetTransactionsAsync())
+                .ReturnsAsync(_fixture.Transactions);
+
+            var repository = new TransactionRepository(AutoMapperSingleton.Mapper, _fixture.MockTransactionService.Object);
+
+            //Act
+            var result = await repository.GetTransactionsAsync();
+
+            //Assert
+            var transactions = result.Should().BeAssignableTo<IEnumerable<GetTransactionDto>>().Subject;
+            transactions.Should().HaveCount(2);
+        }
+
+        [Fact]
+        public async void GetTransactionAsync_Returns_GetTransactionDto()
+        {
+            //Arrange
+            var id = 1;
+            _fixture.MockTransactionService.Setup(x => x.GetTransactionAsync(It.IsAny<Expression<Func<Transaction, bool>>>()))
+                .Returns<Expression<Func<Transaction, bool>>>(expression => Task.FromResult(_fixture.Transactions.AsQueryable().FirstOrDefault(expression)));
+
+            var repository = new TransactionRepository(AutoMapperSingleton.Mapper, _fixture.MockTransactionService.Object);
+
+            //Act
+            var result = await repository.GetTransactionAsync(id);
+
+            //Assert
+            result.Should().BeOfType(typeof(GetTransactionDto));
+            result.Id.Should().Be(id);
+            result.PaymentType.Should().Be("Credit");
+            result.TransactionType.Should().Be("Food");
+            result.Amount.Should().Be(6.5m);
+        }
+
+        [Fact]
+        public async void GetTransactionAsync_Throws_NotFoundException()
+        {
+            //Arrange
+            var id = 201;
+            _fixture.MockTransactionService.Setup(x => x.GetTransactionAsync(It.IsAny<Expression<Func<Transaction, bool>>>()))
+                .Returns<Expression<Func<Transaction, bool>>>(expression => Task.FromResult(_fixture.Transactions.AsQueryable().FirstOrDefault(expression)));
+
+            var repository = new TransactionRepository(AutoMapperSingleton.Mapper, _fixture.MockTransactionService.Object);
+
+            //Act
+            var exception = await Assert.ThrowsAsync<RestException>(() => repository.GetTransactionAsync(id));
+
+            //Assert
+            exception.ErrorCode.Should().Be(HttpStatusCode.NotFound);
+            exception.ErrorMessage.Should().Be("Transaction not found.");
+            exception.ErrorType.Should().Be(HttpStatusCode.NotFound.ToString());
+        }
+
+        [Fact]
+        public async void CreateTransactionAsync_Returns_New_GetTransactionDto()
+        {
+            //Arrange
+            _fixture.MockTransactionService.Setup(x => x.AddTransactionAsync(It.IsAny<Transaction>()))
+                .ReturnsAsync(_fixture.CreatedNewTransaction);
+
+            var repository = new TransactionRepository(AutoMapperSingleton.Mapper, _fixture.MockTransactionService.Object);
+
+            //Act
+            var result = await repository.CreateTransactionAsync(_fixture.CreateTransactionDto);
+
+            //Assert
+            result.Should().BeOfType(typeof(GetTransactionDto));
+            result.Id.Should().Be(3);
+            result.PaymentType.Should().Be("Cash");
+            result.TransactionType.Should().Be("Extra Income");
+            result.Amount.Should().Be(456.5m);
+        }
+
+        [Fact]
+        public async void UpdateTransactionAsync_Returns_Updated_GetTransactionDto()
+        {
+            //Arrange
+            var id = 2;
+            _fixture.MockTransactionService.Setup(x => x.GetTransactionAsync(It.IsAny<Expression<Func<Transaction, bool>>>()))
+                .Returns<Expression<Func<Transaction, bool>>>(expression => Task.FromResult(_fixture.Transactions.AsQueryable().FirstOrDefault(expression)));
+
+            _fixture.MockTransactionService.Setup(x => x.UpdateTransactionAsync(It.IsAny<Transaction>()));
+
+            var repository = new TransactionRepository(AutoMapperSingleton.Mapper, _fixture.MockTransactionService.Object);
+
+            //Act
+            var result = await repository.UpdateTransactionAsync(id, _fixture.EditTransactionDto);
+
+            //Assert
+            result.Should().BeOfType(typeof(GetTransactionDto));
+            result.Id.Should().Be(id);
+            result.PaymentType.Should().Be("Cash");
+            result.TransactionType.Should().Be("Extra Income");
+            result.Amount.Should().Be(10110.5m);
+        }
+
+        [Fact]
+        public async void UpdateTransactionAsync_Throws_NotFoundException()
+        {
+            //Arrange
+            var id = 201;
+            _fixture.MockTransactionService.Setup(x => x.GetTransactionAsync(It.IsAny<Expression<Func<Transaction, bool>>>()))
+                .Returns<Expression<Func<Transaction, bool>>>(expression => Task.FromResult(_fixture.Transactions.AsQueryable().FirstOrDefault(expression)));
+
+            _fixture.MockTransactionService.Setup(x => x.UpdateTransactionAsync(It.IsAny<Transaction>()));
+
+            var repository = new TransactionRepository(AutoMapperSingleton.Mapper, _fixture.MockTransactionService.Object);
+
+            //Act
+            var exception = await Assert.ThrowsAsync<RestException>(() => repository.UpdateTransactionAsync(id, _fixture.EditTransactionDto));
+
+            //Assert
+            exception.ErrorCode.Should().Be(HttpStatusCode.NotFound);
+            exception.ErrorMessage.Should().Be("Transaction not found.");
+            exception.ErrorType.Should().Be(HttpStatusCode.NotFound.ToString());
+        }
+
+        [Fact]
+        public async void DeleteTransactionAsync_Returns_NoResult()
+        {
+            //Arrange
+            var id = 2;
+            _fixture.MockTransactionService.Setup(x => x.GetTransactionAsync(It.IsAny<Expression<Func<Transaction, bool>>>()))
+                .Returns<Expression<Func<Transaction, bool>>>(expression => Task.FromResult(_fixture.Transactions.AsQueryable().FirstOrDefault(expression)));
+
+            _fixture.MockTransactionService.Setup(x => x.DeleteTransactionAsync(It.IsAny<Transaction>()));
+
+            var repository = new TransactionRepository(AutoMapperSingleton.Mapper, _fixture.MockTransactionService.Object);
+
+            //Act
+            await repository.DeleteTransactionAsync(id);
+
+            // Assert
+            _fixture.MockTransactionService.Verify(x => x.DeleteTransactionAsync(It.IsAny<Transaction>()), Times.Once);
+        }
+
+        [Fact]
+        public async void DeleteTransactionAsync_Throws_NotFoundException()
+        {
+            //Arrange
+            var id = 201;
+            _fixture.MockTransactionService.Setup(x => x.GetTransactionAsync(It.IsAny<Expression<Func<Transaction, bool>>>()))
+                .Returns<Expression<Func<Transaction, bool>>>(expression => Task.FromResult(_fixture.Transactions.AsQueryable().FirstOrDefault(expression)));
+
+            _fixture.MockTransactionService.Setup(x => x.DeleteTransactionAsync(It.IsAny<Transaction>()));
+
+            var repository = new TransactionRepository(AutoMapperSingleton.Mapper, _fixture.MockTransactionService.Object);
+
+            //Act
+            var exception = await Assert.ThrowsAsync<RestException>(() => repository.DeleteTransactionAsync(id));
+
+            //Assert
+            exception.ErrorCode.Should().Be(HttpStatusCode.NotFound);
+            exception.ErrorMessage.Should().Be("Transaction not found.");
+            exception.ErrorType.Should().Be(HttpStatusCode.NotFound.ToString());
+        }
+    }
+}
