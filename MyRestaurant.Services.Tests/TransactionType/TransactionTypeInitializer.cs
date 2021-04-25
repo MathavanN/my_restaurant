d81d5129f9e9e298324f9ac26ba1e293193243bf
math2018avan <math2018avan@gmail.com>
2021-04-25 13:51:43 +0800

    TransactionTypeService test cases added

---
+++ MyRestaurant.Services.Tests/TransactionType/TransactionTypeInitializer.cs
@@ -0,0 +1,42 @@
+﻿using MyRestaurant.Core;
+using MyRestaurant.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyRestaurant.Services.Tests
+{
+    public class TransactionTypeInitializer
+    {
+        public static void Initialize(MyRestaurantContext context)
+        {
+            if (!context.TransactionTypes.Any())
+            {
+                var transactionTypes = new List<TransactionType>
+                {
+                    new TransactionType { Type = "Food" },
+                    new TransactionType { Type = "Transportation" },
+                    new TransactionType { Type = "Shopping" },
+                    //new TransactionType { Type = "Mortgage/Rent" },
+                    new TransactionType { Type = "Mortgage" },
+                    new TransactionType { Type = "Clothing" },
+                    new TransactionType { Type = "Housing" },
+                    new TransactionType { Type = "Utilities" },
+                    new TransactionType { Type = "Bills" },
+                    new TransactionType { Type = "Personal Care" },
+                    new TransactionType { Type = "Extra Income" },
+                    new TransactionType { Type = "Miscellaneous" },
+                    new TransactionType { Type = "Health Care" },
+                    new TransactionType { Type = "Interests" },
+                    new TransactionType { Type = "Insurance" },
+                    new TransactionType { Type = "Business" },
+                    new TransactionType { Type = "Tax" },
+                    //new TransactionType { Type = "Salary" },
+                    new TransactionType { Type = "Education" }
+                };
+
+                context.TransactionTypes.AddRange(transactionTypes);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ MyRestaurant.Services.Tests/TransactionType/TransactionTypeServiceTest.cs
@@ -0,0 +1,118 @@
+﻿using FluentAssertions;
+using MyRestaurant.Models;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MyRestaurant.Services.Tests
+{
+    public class TransactionTypeServiceTest : MyRestaurantContextTestBase
+    {
+        public TransactionTypeServiceTest()
+        {
+            TransactionTypeInitializer.Initialize(_myRestaurantContext);
+        }
+
+        [Fact]
+        public async void GetTransactionTypesAsync_Returns_TransactionTypes()
+        {
+            //Arrange
+            var service = new TransactionTypeService(_myRestaurantContext);
+
+            //Act
+            var result = await service.GetTransactionTypesAsync();
+
+            //Assert
+            result.Should().BeAssignableTo<IEnumerable<TransactionType>>();
+            result.Should().HaveCount(17);
+        }
+
+        [Fact]
+        public async void GetTransactionTypeAsync_Returns_TransactionType()
+        {
+            //Arrange
+            var id = 1;
+            var service = new TransactionTypeService(_myRestaurantContext);
+
+            //Act
+            var result = await service.GetTransactionTypeAsync(d => d.Id == id);
+
+            //Assert
+            result.Should().BeAssignableTo<TransactionType>();
+            result.Id.Should().Be(id);
+            result.Type.Should().Be("Food");
+        }
+
+        [Fact]
+        public async void GetTransactionTypeAsync_Returns_Null()
+        {
+            //Arrange
+            var id = 1001;
+            var service = new TransactionTypeService(_myRestaurantContext);
+
+            //Act
+            var result = await service.GetTransactionTypeAsync(d => d.Id == id);
+
+            //Assert
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public async void AddTransactionTypeAsync_Returns_New_TransactionType()
+        {
+            //Arrange
+            var service = new TransactionTypeService(_myRestaurantContext);
+
+            //Act
+            var result = await service.AddTransactionTypeAsync(new TransactionType { Type = "Salary" });
+
+            //Assert
+            result.Should().BeAssignableTo<TransactionType>();
+            result.Type.Should().Be("Salary");
+
+            //Act
+            var transactionTypes = await service.GetTransactionTypesAsync();
+
+            //Assert
+            transactionTypes.Should().HaveCount(18);
+        }
+
+        [Fact]
+        public async void UpdateTransactionTypeAsync_Successfully_Updated()
+        {
+            //Arrange
+            var id = 4;
+            var service = new TransactionTypeService(_myRestaurantContext);
+
+            //Act
+            var dbTransactionType = await service.GetTransactionTypeAsync(d => d.Id == id);
+            dbTransactionType.Type = "Mortgage/Rent";
+
+            await service.UpdateTransactionTypeAsync(dbTransactionType);
+
+            var result = await service.GetTransactionTypeAsync(d => d.Id == id);
+
+            //Assert
+            result.Should().BeAssignableTo<TransactionType>();
+            result.Id.Should().Be(id);
+            result.Type.Should().Be("Mortgage/Rent");
+        }
+
+        [Fact]
+        public async void DeleteTransactionTypeAsync_Successfully_Deleted()
+        {
+            //Arrange
+            var id = 1;
+            var service = new TransactionTypeService(_myRestaurantContext);
+
+            //Act
+            var dbTransactionType = await service.GetTransactionTypeAsync(d => d.Id == id);
+
+            await service.DeleteTransactionTypeAsync(dbTransactionType);
+
+            var result = await service.GetTransactionTypeAsync(d => d.Id == id);
+
+            //Assert
+            result.Should().BeNull();
+        }
+    }
+}
