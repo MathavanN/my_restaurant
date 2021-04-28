ace72035d23418f924fac2230fa0d5c0c107be09
math2018avan <math2018avan@gmail.com>
2021-04-28 11:12:30 +0800

    Transaction dto validator fixture added

---
+++ MyRestaurant.Business.Tests/Validators/V1/Fixtures/Transaction/CreateTransactionDtoValidatorFixture.cs
@@ -0,0 +1,33 @@
+﻿using MyRestaurant.Business.Dtos.V1;
+using MyRestaurant.Business.Validators.V1;
+using System;
+
+namespace MyRestaurant.Business.Tests.Validators.V1.Fixtures
+{
+    public class CreateTransactionDtoValidatorFixture : IDisposable
+    {
+        public CreateTransactionDto Model { get; set; }
+        public CreateTransactionDtoValidator Validator { get; private set; }
+
+        public CreateTransactionDtoValidatorFixture()
+        {
+            Validator = new CreateTransactionDtoValidator();
+            
+            Model = new CreateTransactionDto
+            {
+                TransactionTypeId = 2,
+                PaymentTypeId = 1,
+                Date = DateTime.Now.AddDays(-2),
+                Description = "Interest from Deposit",
+                Amount = 456.5m,
+                Cashflow = "Income"
+            };
+        }
+
+        public void Dispose()
+        {
+            Model = null;
+            Validator = null;
+        }
+    }
+}
