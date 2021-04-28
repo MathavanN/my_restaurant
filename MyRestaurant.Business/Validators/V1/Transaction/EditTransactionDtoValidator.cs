45ad0335946ba09cee0c6993d6dd08118d32dacb
math2018avan <math2018avan@gmail.com>
2021-04-28 11:11:17 +0800

    Dto validation message updated

--- MyRestaurant.Business/Validators/V1/Transaction/EditTransactionDtoValidator.cs
+++ MyRestaurant.Business/Validators/V1/Transaction/EditTransactionDtoValidator.cs
@@ -12,7 +12,7 @@ namespace MyRestaurant.Business.Validators.V1
         {
             RuleFor(x => x.TransactionTypeId).GreaterThan(0).WithMessage("Transaction type is required.");
             RuleFor(x => x.PaymentTypeId).GreaterThan(0).WithMessage("Payment type is required.");
-            RuleFor(x => x.Amount).GreaterThan(0).WithMessage("Amount is required.");
+            RuleFor(x => x.Amount).GreaterThan(0).WithMessage("Amount must be a positive value.");
             RuleFor(x => x.Date).Must(CommonValidators.BeAValidDate).WithMessage("Transaction date is required.")
                 .LessThan(DateTime.Now).WithMessage("Transaction date cannot be a future date.");
             RuleFor(x => x.Cashflow).NotEmpty().WithMessage("Cash flow is required.")
