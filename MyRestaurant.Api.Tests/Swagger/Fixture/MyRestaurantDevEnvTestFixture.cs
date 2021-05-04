cbd8fe8e221774e49542748ecfbae7c6b88e9c0d
math2018avan <math2018avan@gmail.com>
2021-05-04 12:19:48 +0800

    swagger development environment access test case added

---
+++ MyRestaurant.Api.Tests/Swagger/Fixture/MyRestaurantDevEnvTestFixture.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace MyRestaurant.Api.Tests.Swagger.Fixture
+{
+    public class MyRestaurantDevEnvTestFixture : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Development");
+        }
+    }
+}
