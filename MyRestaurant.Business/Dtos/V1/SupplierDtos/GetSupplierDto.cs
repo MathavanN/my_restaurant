ddfdd37a996822c26d0a45777ccbee3a27df0c31
math2018avan <math2018avan@gmail.com>
2021-12-11 17:11:23 +0800

    duplicate code removed
    duplicate code removed

--- MyRestaurant.Business/Dtos/V1/SupplierDtos/GetSupplierDto.cs
+++ MyRestaurant.Business/Dtos/V1/SupplierDtos/GetSupplierDto.cs
@@ -1,17 +1,7 @@
 ﻿namespace MyRestaurant.Business.Dtos.V1
 {
-    public class GetSupplierDto
+    public class GetSupplierDto : SupplierDto
     {
         public long Id { get; set; }
-        public string Name { get; set; } = default!;
-        public string Address1 { get; set; } = default!;
-        public string Address2 { get; set; } = default!;
-        public string City { get; set; } = default!;
-        public string Country { get; set; } = default!;
-        public string Telephone1 { get; set; } = default!;
-        public string Telephone2 { get; set; } = default!;
-        public string Fax { get; set; } = default!;
-        public string Email { get; set; } = default!;
-        public string ContactPerson { get; set; } = default!;
     }
 }
---
+++ MyRestaurant.Business/Dtos/V1/SupplierDtos/SupplierDto.cs
@@ -0,0 +1,16 @@
+﻿namespace MyRestaurant.Business.Dtos.V1
+{
+    public class SupplierDto
+    {
+        public string Name { get; set; } = default!;
+        public string Address1 { get; set; } = default!;
+        public string Address2 { get; set; } = default!;
+        public string City { get; set; } = default!;
+        public string Country { get; set; } = default!;
+        public string? Telephone1 { get; set; }
+        public string? Telephone2 { get; set; }
+        public string? Fax { get; set; }
+        public string? Email { get; set; }
+        public string? ContactPerson { get; set; }
+    }
+}
