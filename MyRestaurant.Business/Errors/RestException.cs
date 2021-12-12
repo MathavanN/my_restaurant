8b6a5f8e742c2d603aa1410c244d5c7bc9b08c1b
math2018avan <math2018avan@gmail.com>
2021-12-12 11:08:10 +0800

    Update RestException.cs
    build issue fixed

--- MyRestaurant.Business/Errors/RestException.cs
+++ MyRestaurant.Business/Errors/RestException.cs
@@ -7,36 +7,36 @@ namespace MyRestaurant.Business.Errors
     public class RestException : Exception, ISerializable
     {
         [NonSerialized]
-        private readonly HttpStatusCode _statusCode;
+        private readonly HttpStatusCode _errorCode;
         [NonSerialized]
-        private readonly string _code;
+        private readonly string _errorType;
         [NonSerialized]
         private readonly string _errorMessage;
 
-        public RestException(HttpStatusCode statusCode, string code, string message) : base(message)
+        public RestException(HttpStatusCode statusCode, string message) : base(message)
         {
-            _statusCode = statusCode;
-            _code = code;
+            _errorCode = statusCode;
+            _errorType = statusCode.ToString();
             _errorMessage = message;
         }
 
+
         protected RestException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
-            _statusCode = (HttpStatusCode)info.GetValue("StatusCode", typeof(HttpStatusCode))!;
-            _code = info.GetString("Code")!;
+            _errorCode = (HttpStatusCode)info.GetValue("ErrorCode", typeof(HttpStatusCode))!;
+            _errorType = info.GetString("ErrorType")!;
             _errorMessage = info.GetString("ErrorMessage")!;
         }
 
-        public HttpStatusCode StatusCode { get { return _statusCode; } }
-        public string Code { get { return _code; } }
+        public HttpStatusCode ErrorCode { get { return _errorCode; } }
+        public string ErrorType { get { return _errorType; } }
         public string ErrorMessage { get { return _errorMessage; } }
-
 
         public override void GetObjectData(SerializationInfo info, StreamingContext context)
         {
-            info.AddValue("Code", _code);
+            info.AddValue("ErrorType", _errorType);
             info.AddValue("ErrorMessage", _errorMessage);
-            info.AddValue("StatusCode", _statusCode, typeof(HttpStatusCode));
+            info.AddValue("ErrorCode", _errorCode, typeof(HttpStatusCode));
             
             // MUST call through to the base class to let it save its own state
             base.GetObjectData(info, context);
